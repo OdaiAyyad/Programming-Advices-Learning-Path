1924b036f57102ddf604665da482be25794c3c63
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-09 22:23:08 +0300

    Variables Practicing

--- 03 - Programming Using C++ - Level 1/Variables Practicing/Variables Practicing.cpp
+++ 03 - Programming Using C++ - Level 1/Variables Practicing/Variables Practicing.cpp
@@ -3,13 +3,29 @@ using namespace std;
 
 int main()
 {
+    cout << "Integer Variables:\n";
     int MyAge;
     MyAge = 50;
     cout << MyAge << endl;
     MyAge = 23;
     cout << MyAge << endl;
-
     cout << "MyAge is" << MyAge << " Years Old!\n\n";
+
+    cout << "Other Variables:\n";
+
+    int MyNumber = 50;
+    float MyFloatNumber = 7.84;
+    double MyDoubleNumber = 21.66659;
+    char MyLetter = 'O';
+    string MyText = "Odai";
+    bool MyBoolean = true;
+
+    cout << MyNumber << endl;
+    cout << MyFloatNumber << endl;
+    cout << MyDoubleNumber << endl;
+    cout << MyLetter << endl;
+    cout << MyText << endl;
+    cout << MyBoolean << endl;
 
     return 0;
 }
