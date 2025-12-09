61f05c54d0194bf4ba5bf4de06d4c8e7548785da
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-09 22:09:54 +0300

    Variables Practicing

---
+++ 03 - Programming Using C++ - Level 1/Variables Practicing/Variables Practicing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int MyAge;
+    MyAge = 50;
+    cout << MyAge << endl;
+    MyAge = 23;
+    cout << MyAge << endl;
+
+    return 0;
+}
