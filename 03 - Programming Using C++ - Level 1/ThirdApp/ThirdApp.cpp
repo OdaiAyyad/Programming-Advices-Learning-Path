b79a52f1f6c60ddc0cb774d11bbf531221c5e18f
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-09 15:09:50 +0300

    Third App

---
+++ 03 - Programming Using C++ - Level 1/ThirdApp/ThirdApp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Odai Ayyad";
+    std::cout << "This is My Third C++ Program";
+
+    std::cout << "\nOdai Ayyad\n\n";
+    std::cout << "This is My Third C++ Program\n";
+
+    std::cout << "Odai Ayyad" << std::endl;
+    std::cout << "This is My Third C++ Program" << std::endl;
+
+    std::cout << "M1" << "M2" << "M3\n";
+    std::cout << "M1 " << "M2 " << "M3\n";
+
+    std::cout << "M1" << 10+5 << "M3\n";
+    std::cout << "M1 " << "M2 " << "M3\n";
+
+    std::cout << "M1 " << 10+5 << " M3\n";
+    std::cout << "M1 " << "M2 " << "M3\n";
+
+    std::cout << "The Result of 10+5= " << 10 + 5 << "\n";
+
+    return 0;
+}
