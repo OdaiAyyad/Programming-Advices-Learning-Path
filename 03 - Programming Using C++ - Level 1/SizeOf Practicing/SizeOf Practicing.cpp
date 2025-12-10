eb3ac3148fab190a558ac916dada960de140f515
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-10 15:44:30 +0300

    SizeOf Practicing

---
+++ 03 - Programming Using C++ - Level 1/SizeOf Practicing/SizeOf Practicing.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "The size of bool data type is " << sizeof(bool) << "\n";
+    cout << "The size of char data type is " << sizeof(char) << "\n";
+    cout << "The size of short data type is " << sizeof(short int) << "\n";
+    cout << "The size of int data type is " << sizeof(int) << "\n";
+
+    cout << "The size of int long data type is " << sizeof(long) << "\n";
+    cout << "The size of int long long data type is " << sizeof(long long) << "\n";
+
+    cout << "The size of float data type is " << sizeof(float) << "\n";
+    cout << "The size of double data type is " << sizeof(double) << "\n";
+
+    return 0;
+}
