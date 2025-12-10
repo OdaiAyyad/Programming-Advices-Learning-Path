9a67867908e458980774decde8be793e211245f8
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-10 17:48:00 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Information Card H.W/Information Card H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Information Card H.W/Information Card H.W.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "**********************\n";
-    std::cout << "Name: Odai Ayyad.\n";
-    std::cout << "Age: 23 Years.\n";
-    std::cout << "Country: Jordan.\n";
-    std::cout << "City: Amman.\n";
-    std::cout << "**********************\n";
+    cout << "**********************\n";
+    cout << "Name: Odai Ayyad.\n";
+    cout << "Age: 23 Years.\n";
+    cout << "Country: Jordan.\n";
+    cout << "City: Amman.\n";
+    cout << "**********************\n";
 
     return 0;
 }
