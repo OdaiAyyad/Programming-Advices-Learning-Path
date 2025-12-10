0f2a8b4828d7659fe5638779c0896efe7de9f6e8
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-10 12:33:21 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Input Age Variable H.W/Input Age Variable H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Input Age Variable H.W/Input Age Variable H.W.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main()
 {
-    int Age = 25;
+    int Age;
+
+    cout << "Enter your age: "; cin >> Age;
 
     cout << "After 5 years you will be " << Age + 5 << " years old.\n";
 
