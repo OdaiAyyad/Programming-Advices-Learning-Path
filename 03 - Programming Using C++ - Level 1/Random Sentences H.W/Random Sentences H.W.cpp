3c92bc1be7fe7e81b63cc1188346eb7f48ce7284
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-10 17:39:19 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Random Sentences H.W/Random Sentences H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Random Sentences H.W/Random Sentences H.W.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "I Love Programming!\n\n";
+    cout << "I Love Programming!\n\n";
 
-    std::cout << "I Promise to be the best developer ever!\n\n";
+    cout << "I Promise to be the best developer ever!\n\n";
 
-    std::cout << "I know it will take some time to practice, but I will achieve my goal.\n\n";
+    cout << "I know it will take some time to practice, but I will achieve my goal.\n\n";
 
-    std::cout << "Best Regards,\n";
-    std::cout << "Odai Ayyad\n";
+    cout << "Best Regards,\n";
+    cout << "Odai Ayyad\n";
 
     return 0;
 }
