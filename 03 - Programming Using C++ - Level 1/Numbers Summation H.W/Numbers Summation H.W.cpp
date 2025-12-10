e1daa3ec94a7104f124056f87e2aeb3d84ef193a
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-10 17:41:22 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Numbers Summation H.W/Numbers Summation H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Numbers Summation H.W/Numbers Summation H.W.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int Num1 = 20, Num2 = 30, Num3 = 10;
+    short Num1 = 20, Num2 = 30, Num3 = 10;
 
     cout << Num1 << " +\n" << Num2 << " +\n" << Num3;
     cout << "\n___________________________________\n";
