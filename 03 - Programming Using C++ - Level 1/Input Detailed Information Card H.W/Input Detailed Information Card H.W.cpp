a88119358a713aab04f9d6423c7196cfed9ba3b6
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-10 12:24:28 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Input Detailed Information Card H.W/Input Detailed Information Card H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Input Detailed Information Card H.W/Input Detailed Information Card H.W.cpp
@@ -1,20 +1,38 @@
-// Input Detailed Information Card H.W.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string Name;
+    int Age;
+    string Country;
+    string City;
+    float MonthlySalary;
+    char Gender;
+    bool Married;
+
+    cout << "Please, fill out the following information about you:\n";
+    cout << "Name: "; cin >> Name;
+    cout << "\nAge: "; cin >> Age;
+    cout << "\nCountry:"; cin >> Country;
+    cout << "\nCity: "; cin >> City;
+    cout << "\nMonthly Salary: "; cin >> MonthlySalary;
+    float YearlySalary = MonthlySalary * 12;
+    cout << "\nGender: "; cin >> Gender;
+    cout << "\nAre you married? "; cin >> Married;
+
+    cout << endl;
+
+    cout << "**********************\n";
+    cout << "Name: " << Name << endl;
+    cout << "Age: " << Age << endl;
+    cout << "Country: " << Country << endl;
+    cout << "City: " << City << endl;
+    cout << "Monthly Salary: " << MonthlySalary << endl;
+    cout << "Yearly Salary: " << YearlySalary << endl;
+    cout << "Gender: " << Gender << endl;
+    cout << "Married? " << Married << endl;
+    cout << "**********************\n";
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
