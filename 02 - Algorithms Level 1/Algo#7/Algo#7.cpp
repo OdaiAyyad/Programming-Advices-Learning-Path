5258dc7670ff5eb974c5faf02fb6fe553a746221
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-10 19:04:46 +0300

    Algo#7

---
+++ 02 - Algorithms Level 1/Algo#7/Algo#7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Num;
+
+    cout << "Enter a Number: "; cin >> Num;
+
+    cout << "Half of " << Num << " is " << Num / 2 << endl;
+
+    return 0;
+}
