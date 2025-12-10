489d4a564019fb51b6db9947234a9b33db993d7f
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-10 19:54:12 +0300

    Algo#15

---
+++ 02 - Algorithms Level 1/Algo#15/Algo#15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int a, b;
+
+    cout << "Enter first line length: "; cin >> a;
+    cout << "Enter second line length: "; cin >> b;
+
+    cout << "\nArea = " << a * b << endl;
+
+    return 0;
+}
