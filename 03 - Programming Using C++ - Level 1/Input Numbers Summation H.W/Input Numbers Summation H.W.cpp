1a8f3f10bfc0c474b31b270bee0b9588982698ba
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-10 12:30:59 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Input Numbers Summation H.W/Input Numbers Summation H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Input Numbers Summation H.W/Input Numbers Summation H.W.cpp
@@ -1,20 +1,19 @@
-// Input Numbers Summation H.W.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int Num1, Num2, Num3;
+
+    cout << "Enter Number1: "; cin >> Num1;
+    cout << "Enter Number2: "; cin >> Num2;
+    cout << "Enter Number3: "; cin >> Num3;
+
+    cout << endl;
+
+    cout << Num1 << " +\n" << Num2 << " +\n" << Num3;
+    cout << "\n___________________________________\n";
+    cout << "Total = " << Num1 + Num2 + Num3 << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
