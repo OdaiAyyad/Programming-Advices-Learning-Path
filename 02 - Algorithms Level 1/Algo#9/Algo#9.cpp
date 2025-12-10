322f67414e8501ea81e9309bb672df499bc2dffc
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-10 19:08:21 +0300

    Algo#9

---
+++ 02 - Algorithms Level 1/Algo#9/Algo#9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, num3;
+
+    cout << "Enter the first number: "; cin >> num1;
+    cout << "Enter the second number: "; cin >> num2;
+    cout << "Enter the third number: "; cin >> num3;
+
+    cout << "Total = " << num1 + num2 + num3 << endl;
+
+    return 0;
+}
