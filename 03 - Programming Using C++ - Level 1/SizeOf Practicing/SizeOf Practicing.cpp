f1782b4bb2f891f047aacef4ee9ec2c1a7661d98
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-10 15:47:25 +0300

    SizeOf Practicing

--- 03 - Programming Using C++ - Level 1/SizeOf Practicing/SizeOf Practicing.cpp
+++ 03 - Programming Using C++ - Level 1/SizeOf Practicing/SizeOf Practicing.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-    cout << "The size of bool data type is " << sizeof(bool) << "\n";
-    cout << "The size of char data type is " << sizeof(char) << "\n";
-    cout << "The size of short data type is " << sizeof(short int) << "\n";
-    cout << "The size of int data type is " << sizeof(int) << "\n";
+    cout << "The size of bool data type is " << sizeof(bool) << " byte\n";
+    cout << "The size of char data type is " << sizeof(char) << " byte\n";
+    cout << "The size of short data type is " << sizeof(short int) << " byte\n";
+    cout << "The size of int data type is " << sizeof(int) << " byte\n";
 
-    cout << "The size of int long data type is " << sizeof(long) << "\n";
-    cout << "The size of int long long data type is " << sizeof(long long) << "\n";
+    cout << "The size of int long data type is " << sizeof(long) << " byte\n";
+    cout << "The size of int long long data type is " << sizeof(long long) << " byte\n";
 
-    cout << "The size of float data type is " << sizeof(float) << "\n";
-    cout << "The size of double data type is " << sizeof(double) << "\n";
+    cout << "The size of float data type is " << sizeof(float) << " byte\n";
+    cout << "The size of double data type is " << sizeof(double) << " byte\n";
 
     return 0;
 }
