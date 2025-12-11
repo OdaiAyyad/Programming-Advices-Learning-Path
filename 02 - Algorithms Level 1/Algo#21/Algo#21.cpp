bc9a955d9d086eabed8ce9ebe18b7061e084c285
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-11 22:50:06 +0300

    Algo#21

--- 02 - Algorithms Level 1/Algo#21/Algo#21.cpp
+++ 02 - Algorithms Level 1/Algo#21/Algo#21.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main()
 {
     int L;
+    const float PI = 3.14;
 
     cout << "Enter circle circumference: "; cin >> L;
 
-    cout << "Area = " << (L * L) << endl;
+    cout << "Area = " << (L * L) / (4 * PI) << endl;
 
     return 0;
 }
