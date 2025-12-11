54a04730f2f84fa0d9e8607747afdfde2e3b56f9
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-12 00:22:48 +0300

    Algo#43

--- 02 - Algorithms Level 1/Algo#43/Algo#43.cpp
+++ 02 - Algorithms Level 1/Algo#43/Algo#43.cpp
@@ -1,20 +1,25 @@
-// Algo#43.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int TotalSeconds, Remainder;
+
+    cout << "Enter your task total time duration in seconds: "; cin >> TotalSeconds;
+
+    int SecondsPerMinute = 60;
+    int SecondsPerHour = 60 * 60;
+    int SecondsPerDay = 60 * 60 * 24;
+
+    int NumberOfDays = floor(TotalSeconds / SecondsPerDay);
+    Remainder = TotalSeconds % SecondsPerDay;
+    int NumberOfHours = floor(Remainder / SecondsPerMinute);
+    Remainder = Remainder % SecondsPerMinute;
+    int NumberOfMinutes = floor(Remainder / SecondsPerMinute);
+    Remainder = Remainder % SecondsPerMinute;
+    int NumberOfSeconds = Remainder;
+
+    cout << SecondsPerDay << ":" << SecondsPerMinute << ":" << SecondsPerMinute << ":" << NumberOfSeconds;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
