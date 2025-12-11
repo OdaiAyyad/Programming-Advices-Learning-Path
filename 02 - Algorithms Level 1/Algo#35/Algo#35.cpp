8e04883a2058b83c2c53eda19413e443b6fcdf63
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-11 23:26:23 +0300

    Algo#35

--- 02 - Algorithms Level 1/Algo#35/Algo#35.cpp
+++ 02 - Algorithms Level 1/Algo#35/Algo#35.cpp
@@ -6,6 +6,10 @@ int main()
     unsigned int Penny, Nickel, Dime, Quarter, Dollar;
 
     cout << "Enter the number of Pennies: "; cin >> Penny;
+    cout << "Enter the number of Nickels: "; cin >> Nickel;
+    cout << "Enter the number of Dimes: "; cin >> Dime;
+    cout << "Enter the number of Quarters: "; cin >> Quarter;
+    cout << "Enter the number of Dollars: "; cin >> Dollar;
 
     return 0;
 }
