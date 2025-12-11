7f3f5260cf37cd235a19efffb8f6ac798333e55a
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-11 23:07:35 +0300

    Algo#31

--- 02 - Algorithms Level 1/Algo#31/Algo#31.cpp
+++ 02 - Algorithms Level 1/Algo#31/Algo#31.cpp
@@ -1,20 +1,15 @@
-// Algo#31.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a, b;
+    const float PI = 3.14;
+
+    cout << "Enter triangle side length: "; cin >> a;
+    cout << "Enter triangle base length: "; cin >> b;
+    float A = PI * ((b * b) / 4) * ((2 * a - b) / (2 * a + b));
+    cout << "Area = " << A << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
