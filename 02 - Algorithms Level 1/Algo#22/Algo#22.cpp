c080fd682a5e41224b75bee520bc6e0d6eabc5b6
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-11 22:54:59 +0300

    Algo#22

--- 02 - Algorithms Level 1/Algo#22/Algo#22.cpp
+++ 02 - Algorithms Level 1/Algo#22/Algo#22.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main()
 {
+    int a, b;
+    const float PI = 3.14;
 
+    cout << "Enter triangle side length: "; cin >> a;
+
+    cout << "Area = " << (a * a) / (4 * PI) << endl;
 
     return 0;
 }
