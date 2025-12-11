c6a9ef337785f7469cac42ee2539c902deeac943
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-11 23:11:30 +0300

    Algo#31

--- 02 - Algorithms Level 1/Algo#31/Algo#31.cpp
+++ 02 - Algorithms Level 1/Algo#31/Algo#31.cpp
@@ -3,13 +3,9 @@ using namespace std;
 
 int main()
 {
-    int a, b;
-    const float PI = 3.14;
+    int N;
 
-    cout << "Enter triangle side length: "; cin >> a;
-    cout << "Enter triangle base length: "; cin >> b;
-    float A = PI * ((b * b) / 4) * ((2 * a - b) / (2 * a + b));
-    cout << "Area = " << A << endl;
+    cout << "Enter a Number: "; cin >> N;
 
     return 0;
 }
