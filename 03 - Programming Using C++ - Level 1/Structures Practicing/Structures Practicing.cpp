115ad1d661433c87527726ad9279581d913aaafd
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-12 20:55:54 +0300

    Structures Practicing

--- 03 - Programming Using C++ - Level 1/Structures Practicing/Structures Practicing.cpp
+++ 03 - Programming Using C++ - Level 1/Structures Practicing/Structures Practicing.cpp
@@ -2,11 +2,19 @@
 #include <cmath>
 using namespace std;
 
+struct StOwner
+{
+    string FullName;
+    string Phone;
+    string Address;
+};
+
 struct Car
 {
     string Brand;
     string Model;
     int YearOfMade;
+    StOwner Owner;
 };
 
 int main()
@@ -16,12 +24,15 @@ int main()
     car1.Brand = "Mercedes";
     car1.Model = "AMG GT";
     car1.YearOfMade = 2020;
+    car1.Owner.FullName = "Odai Ayyad";
+    car1.Owner.Phone = "0123456789";
 
     car2.Brand = "BMW";
     car2.Model = "M5";
     car2.YearOfMade = 2018;
 
     cout << car1.Brand << " " << car1.Model << " " << car1.YearOfMade << endl;
+    cout << car1.Owner.FullName << " " << car1.Owner.Phone << endl << endl;
     cout << car2.Brand << " " << car2.Model << " " << car2.YearOfMade << endl;
 
     return 0;
