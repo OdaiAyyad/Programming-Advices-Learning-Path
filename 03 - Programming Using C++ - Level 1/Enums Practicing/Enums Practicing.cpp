b389ee178875e56b2486e948afa963991aea1979
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-12 21:48:41 +0300

    Enums Practicing

--- 03 - Programming Using C++ - Level 1/Enums Practicing/Enums Practicing.cpp
+++ 03 - Programming Using C++ - Level 1/Enums Practicing/Enums Practicing.cpp
@@ -2,15 +2,31 @@
 #include <cmath>
 using namespace std;
 
-enum Colors { Red, Green, Blue};
+enum Color { Red, Green, Blue }; // {0, 1, 2...} By Default
+enum Direction { North, South, East, West };
+enum Week { Sat, Sun, Mon, Tue, Wed, Thu, Fri };
+enum Gender { Male, Female };
+enum Status { Single = 100, Married = 300 }; // Assigning Values Manually
 
 int main()
 {
-    Colors Color1;
+    Color MyColor;
+    Direction MyDirection;
+    Week Today;
+    Gender MyGender;
+    Status MyStatus;
 
-    Color1 = Colors::Blue;
+    MyColor = Color::Blue;
+    MyDirection = Direction::East;
+    Today = Week::Fri;
+    MyGender = Gender::Male;
+    MyStatus = Status::Single;
 
-    cout << "Color is: " << Color1 << endl;
+    cout << "Color is: " << MyColor << endl;
+    cout << "Direction is: " << MyDirection << endl;
+    cout << "Today is: " << Today << endl;
+    cout << "Gender is: " << MyGender << endl;
+    cout << "Status is: " << MyStatus << endl;
 
     return 0;
 }
