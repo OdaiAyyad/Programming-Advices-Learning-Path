ae6ead17bd01ba9cfdc07efd5f42fb3a0ddd4b64
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-12 16:00:30 +0300

    Math Functions Practicing

--- 03 - Programming Using C++ - Level 1/Math Functions Prcticing/Math Functions Prcticing.cpp
+++ 03 - Programming Using C++ - Level 1/Math Functions Prcticing/Math Functions Prcticing.cpp
@@ -5,9 +5,15 @@ using namespace std;
 int main()
 {
     // Square Root (sqrt)
-    cout << "Square root value of 64  = " << sqrt(64) << endl;
-    cout << "Square root value of 50  = " << sqrt(50) << endl;
+    cout << "Square root value of 64 = " << sqrt(64) << endl;
+    cout << "Square root value of 50 = " << sqrt(50) << endl;
 
+    // Round
+    cout << "Round(2.4) = " << round(2.4) << endl;
+    cout << "Round(2.5) = " << round(2.5) << endl;
+    cout << "Round(2.7) = " << round(2.7) << endl;
+
+    cout << "Round of square root value of 50 = " << round(sqrt(50)) << endl;
 
     return 0;
 }
