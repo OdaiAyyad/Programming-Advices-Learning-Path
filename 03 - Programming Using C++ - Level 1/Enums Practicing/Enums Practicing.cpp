fbf3017e0b322bd8fe670e5b51398bfed887f99b
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-12 21:39:20 +0300

    Enums Practicing

--- 03 - Programming Using C++ - Level 1/Enums Practicing/Enums Practicing.cpp
+++ 03 - Programming Using C++ - Level 1/Enums Practicing/Enums Practicing.cpp
@@ -2,9 +2,15 @@
 #include <cmath>
 using namespace std;
 
+enum Colors { Red, Green, Blue};
+
 int main()
 {
+    Colors Color1;
 
+    Color1 = Colors::Blue;
+
+    cout << "Color is: " << Color1 << endl;
 
     return 0;
 }
