895dde1c1f89818d473d7372887741a5d3bfe03f
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-12 22:02:29 +0300

    H.W

---
+++ 03 - Programming Using C++ - Level 1/Enums H.W/Enums H.W.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+enum Gender { Male, Female };
+enum Status { Single, Married };
+enum FavColor { Red, Green ,Yellow, Blue, Black, White, Pink, Purple, Brown };
+
+struct StAddress
+{
+    string Country;
+    string City;
+    string Street;
+    string PoBox;
+};
+
+struct StSalary
+{
+    float MonthlySalary;
+    float YearlySalary;
+};
+
+struct StPerson
+{
+    string FullName;
+    short Age;
+    Status Status;
+    Gender Gender;
+    string Phone;
+    StAddress Address;
+    StSalary Salary;
+    FavColor FavColor;
+};
+
+int main()
+{
+    StPerson person1;
+
+    person1.FullName = "Odai Ayyad";
+    person1.Age = 23;
+    person1.Gender = Gender::Male;
+    person1.Status = Status::Single;
+    person1.Salary.MonthlySalary = 7500;
+    person1.Salary.YearlySalary = person1.Salary.MonthlySalary * 12;
+    person1.Address.Country = "Jordan";
+    person1.Address.City = "Amman";
+    person1.FavColor = FavColor::Blue;
+
+    cout << "**********************\n";
+    cout << "Name: " << person1.FullName << endl;
+    cout << "Age: " << person1.Age << endl;
+    cout << "Married? " << person1.Status << endl;
+    cout << "Gender: " << person1.Gender << endl;
+    cout << "Country: " << person1.Address.Country << endl;
+    cout << "City: " << person1.Address.City << endl;
+    cout << "Monthly Salary: " << person1.Salary.MonthlySalary << endl;
+    cout << "Yearly Salary: " << person1.Salary.YearlySalary << endl;
+    cout << "Favoruit Color: " << person1.FavColor << endl;
+    cout << "**********************\n";
+
+    return 0;
+}
