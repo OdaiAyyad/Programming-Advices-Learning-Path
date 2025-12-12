677c7a7dfc505e1abb803f724dbb1ef81e802fbd
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-12 20:43:25 +0300

    Structures Practicing

--- 03 - Programming Using C++ - Level 1/Structures Practicing/Structures Practicing.cpp
+++ 03 - Programming Using C++ - Level 1/Structures Practicing/Structures Practicing.cpp
@@ -11,13 +11,18 @@ struct Car
 
 int main()
 {
-    Car car1;
+    Car car1, car2;
 
     car1.Brand = "Mercedes";
     car1.Model = "AMG GT";
     car1.YearOfMade = 2020;
 
+    car2.Brand = "BMW";
+    car2.Model = "M5";
+    car2.YearOfMade = 2018;
+
     cout << car1.Brand << " " << car1.Model << " " << car1.YearOfMade << endl;
+    cout << car2.Brand << " " << car2.Model << " " << car2.YearOfMade << endl;
 
     return 0;
 }
