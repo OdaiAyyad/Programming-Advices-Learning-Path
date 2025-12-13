905a6d54d141cc0113b38d42e6b01d9794315386
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-13 21:55:47 +0300

    Functions Part 2

--- 03 - Programming Using C++ - Level 1/Functions Part 2/Functions Part 2.cpp
+++ 03 - Programming Using C++ - Level 1/Functions Part 2/Functions Part 2.cpp
@@ -16,10 +16,19 @@ string MyFunction2()
     return "This is my first returning value function, this is the value.\n";
 }
 
+int MyFunction3()
+{
+    int x = 10;
+    int y = 20;
+
+    return x * y;
+}
+
 int main()
 {
     MyFunction();
     cout << MyFunction2() << endl;
+    cout << MyFunction3() << endl;
 
     return 0;
 }
