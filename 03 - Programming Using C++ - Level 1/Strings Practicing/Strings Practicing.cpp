9a7d2d0fa9da451cd5d6f1f68801a4cc4605d9d3
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-13 19:05:20 +0300

    Strings Practicing

--- 03 - Programming Using C++ - Level 1/Strings Practicing/Strings Practicing.cpp
+++ 03 - Programming Using C++ - Level 1/Strings Practicing/Strings Practicing.cpp
@@ -11,6 +11,12 @@ int main()
     cout << MyString[0] << endl; // Will print A
     cout << MyString[2] << endl; // Will print C
 
+    string S1 = "10", S2 = "20";
+    string S3 = S1 + S2; // Concatenation
+
+    cout << S3 << endl; // Will print 1020
+
+
 
     return 0;
 }
