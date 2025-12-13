b3e45703c45e3e71b593c845b1141153d4357745
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-13 14:13:23 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Data Type Conversion H.W/Data Type Conversion H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Data Type Conversion H.W/Data Type Conversion H.W.cpp
@@ -1,20 +1,20 @@
-// Data Type Conversion H.W.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string st1 = "43.22";
+
+    double num_d = stod(st1);
+    float num_f = stof(st1);
+    int num_i = stoi(st1);
+
+    cout << "Double Number: " << num_d << endl;
+    cout << "Float Number: " << num_f << endl;
+    cout << "Integer Number: " << num_i << endl;
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
