2ce1c7b1a197ca4104e16688d5c3dfdd92cb52c0
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-13 12:40:43 +0300

    Nested Structures and Enums

--- 03 - Programming Using C++ - Level 1/Nested Structures and Enums/Nested Structures and Enums.cpp
+++ 03 - Programming Using C++ - Level 1/Nested Structures and Enums/Nested Structures and Enums.cpp
@@ -8,12 +8,47 @@ enum enMartialStatus { Single, Married };
 
 struct stAddress
 {
+    string Country;
+    string City;
+    string Street;
+    string PoBox;
+};
 
+struct stContactInfo
+{
+    string Phone;
+    string Email;
+    stAddress Address;
+};
+
+struct stPerson
+{
+    string FirstName;
+    string LastName;
+    stContactInfo ContactInfo;
+
+    enColor FavColor;
+    enMartialStatus MartialStatus;
+    enGender Gender;
 };
 
 int main()
 {
+    stPerson person1;
 
+    person1.FirstName = "Odai";
+    person1.LastName = "Ayyad";
+
+    person1.ContactInfo.Email = "abcd@gmail.com";
+    person1.ContactInfo.Phone = "0123456789";
+    person1.ContactInfo.Address.Country = "Jordan";
+    person1.ContactInfo.Address.City = "Amman";
+    person1.ContactInfo.Address.PoBox = "62001";
+    person1.ContactInfo.Address.Street = "L8998";
+
+    person1.Gender = enGender::Male;
+    person1.FavColor = enColor::Blue;
+    person1.MartialStatus = enMartialStatus::Single;
 
     return 0;
 }
