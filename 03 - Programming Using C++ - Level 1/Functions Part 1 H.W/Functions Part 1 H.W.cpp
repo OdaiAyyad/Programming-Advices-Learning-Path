44182110e2b0acbad53dacc73775ed59a49c6d8f
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-13 21:29:31 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Functions Part 1 H.W/Functions Part 1 H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Functions Part 1 H.W/Functions Part 1 H.W.cpp
@@ -13,9 +13,33 @@ void InformationCard()
     cout << "**********************\n";
 }
 
+void StarsRows()
+{
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n";
+    cout << "********\n";
+}
+
+void RandomSentences()
+{
+    cout << "I Love Programming!\n\n";
+
+    cout << "I Promise to be the best developer ever!\n\n";
+
+    cout << "I know it will take some time to practice, but I will achieve my goal.\n\n";
+
+    cout << "Best Regards,\n";
+    cout << "Odai Ayyad\n";
+}
+
 int main()
 {
     InformationCard();
+    cout << endl;
+    StarsRows();
+    cout << endl;
+    RandomSentences();
     cout << endl;
 
     return 0;
