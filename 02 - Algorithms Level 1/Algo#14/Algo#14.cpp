1bdd5924643df86c8b109511ea9a5a4a0ac95444
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-13 23:14:13 +0300

    Algo#14

--- 02 - Algorithms Level 1/Algo#14/Algo#14.cpp
+++ 02 - Algorithms Level 1/Algo#14/Algo#14.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void Swap(int num1, int num2)
+{
+    int temp;
+
+    temp = num2;
+    num2 = num1;
+    num1 = temp;
+
+    cout << "Num1 = " << num1 << endl;
+    cout << "Num2 = " << num2 << endl;
+}
+
 int main()
 {
-    int num1, num2, temp;
+    int num1, num2;
 
     cout << "Enter the first number: "; cin >> num1;
     cout << "Enter the second number: "; cin >> num2;
@@ -11,13 +23,8 @@ int main()
     cout << "Num1 = " << num1 << endl;
     cout << "Num2 = " << num2 << endl;
 
-    temp = num2;
-    num2 = num1;
-    num1 = temp;
-
     cout << "\nAfter Swapping:\n";
-    cout << "Num1 = " << num1 << endl;
-    cout << "Num2 = " << num2 << endl;
+    Swap(num1, num2);
 
     return 0;
 }
