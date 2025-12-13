193187684bd96e160af66ea356a6fb4e542de861
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-13 22:09:33 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Functions Part 2 H.W/Functions Part 2 H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Functions Part 2 H.W/Functions Part 2 H.W.cpp
@@ -1,20 +1,36 @@
-// Functions Part 2 H.W.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+void MySumProcedure()
+{
+    int num1, num2, result;
+    cout << "Enter the first number: "; cin >> num1;
+    cout << "Enter the second number: "; cin >> num2;
+    cout << "********************************************\n";
+
+    result = num1 + num2;
+    cout << result;
+}
+
+int MySumFunction()
+{
+    int num1, num2, result;
+    cout << "Enter the first number: "; cin >> num1;
+    cout << "Enter the second number: "; cin >> num2;
+    cout << "********************************************\n";
+
+    result = num1 + num2;
+    
+    return result;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    MySumProcedure();
+    cout << endl;
+    cout << MySumFunction() << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
