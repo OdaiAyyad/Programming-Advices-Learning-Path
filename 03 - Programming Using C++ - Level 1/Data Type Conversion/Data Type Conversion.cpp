1f486396fa5a9ca7d7bfdfaf2d8f3ea46060ff4a
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-13 13:56:56 +0300

    Data Type Conversion

--- 03 - Programming Using C++ - Level 1/Data Type Conversion/Data Type Conversion.cpp
+++ 03 - Programming Using C++ - Level 1/Data Type Conversion/Data Type Conversion.cpp
@@ -9,9 +9,9 @@ int main()
 
     // num1 = num2; // Implicit conversion
 
-    num1 = (int)num2; // Explicit Conversion
+    // num1 = (int)num2; // Explicit Conversion
 
-    // num1 = int(num2); // Explicit Conversion
+    num1 = int(num2); // Explicit Conversion
 
     cout << num1 << endl;
 
