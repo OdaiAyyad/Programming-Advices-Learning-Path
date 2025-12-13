b490cba8a0c7389f42a914f9617e72b2ee007105
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-13 23:02:36 +0300

    Functions Part 3

--- 03 - Programming Using C++ - Level 1/Functions Part 3/Functions Part 3.cpp
+++ 03 - Programming Using C++ - Level 1/Functions Part 3/Functions Part 3.cpp
@@ -5,15 +5,17 @@ using namespace std;
 
 int MySumFunction(int num1, int num2)
 {
-    //cout << "Enter the first number: "; cin >> num1;
-    //cout << "Enter the second number: "; cin >> num2;
-
     return num1 + num2;
 }
 
 int main()
 {
-    cout << MySumFunction(10, 20) << endl;
+    int num1, num2;
+
+    cout << "Enter the first number: "; cin >> num1;
+    cout << "Enter the second number: "; cin >> num2;
+
+    cout << "Result = " << MySumFunction(num1, num2) << endl;
 
     return 0;
 }
