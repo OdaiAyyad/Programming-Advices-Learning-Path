9092379645dd5498ec35611f9bf9580192d8e23f
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-13 21:54:05 +0300

    Functions Part 2

--- 03 - Programming Using C++ - Level 1/Functions Part 2/Functions Part 2.cpp
+++ 03 - Programming Using C++ - Level 1/Functions Part 2/Functions Part 2.cpp
@@ -1,20 +1,25 @@
-// Functions Part 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+// Any function with (void) data type, doesn't return any value
+// // Any function with (void) data type, we called it a (Procedure)
+
+void MyFunction()
+{
+    cout << "This is my first function, it got executed :-)\n";
+}
+
+string MyFunction2()
+{
+    return "This is my first returning value function, this is the value.\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    MyFunction();
+    cout << MyFunction2() << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
