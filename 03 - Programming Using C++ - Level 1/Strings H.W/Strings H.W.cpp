bd5d4dbf57ab84f372e7b41c84f13f9ec35e56e1
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-13 19:20:16 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Strings H.W/Strings H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Strings H.W/Strings H.W.cpp
@@ -8,12 +8,18 @@ int main()
     string S1, S2, S3;
     cout << "Enter the first string: ";
     getline(cin, S1);
+    cout << S1 << "\n";
 
-    cout << "\nEnter the second string: ";
+    cout << "Enter the second string: ";
     getline(cin, S2);
+    cout << S2 << "\n";
 
-    cout << "\nEnter the third string: ";
+    cout << "Enter the third string: ";
     getline(cin, S3);
+    cout << S3 << "\n";
+
+    cout << "The length of the first string is: " << S1.length() << endl;
+    cout << "Characters at 0, 2, 4, 7 are: " << S1[0] << ", " << S1[2] << ", " << S1[4] << ", " << S1[7] << "\n\n";
 
 
 
