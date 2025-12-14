33748d57bc1431794b07aa75935cf17ebfaa5bb4
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-14 17:01:00 +0300

    Algo#22

--- 02 - Algorithms Level 1/Algo#22/Algo#22.cpp
+++ 02 - Algorithms Level 1/Algo#22/Algo#22.cpp
@@ -2,16 +2,23 @@
 #include <cmath>
 using namespace std;
 
+float Area(float a, float b)
+{
+    const float PI = 3.14;
+
+    float Area = (PI * pow(b, 2) / 4) * ((2 * a - b) / (2 * a + b));
+
+    return Area;
+}
+
 int main()
 {
     float a, b;
-    const float PI = 3.14;
 
     cout << "Enter triangle side length: "; cin >> a;
     cout << "Enter triangle base length: "; cin >> b;
  
-    float Area = (PI * pow(b, 2) / 4) * ((2 * a - b) / (2 * a + b));
-    cout << "Area = " << floor(Area) << endl;
+    cout << "Area = " << floor(Area(a, b)) << endl;
 
     return 0;
 }
