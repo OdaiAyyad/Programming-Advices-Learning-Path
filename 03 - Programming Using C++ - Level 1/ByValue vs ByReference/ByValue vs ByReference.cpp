4830b099f8d67252236ed44964aae559283f72ad
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-14 20:05:22 +0300

    ByValue vs ByReference

--- 03 - Programming Using C++ - Level 1/ByValue vs ByReference/ByValue vs ByReference.cpp
+++ 03 - Programming Using C++ - Level 1/ByValue vs ByReference/ByValue vs ByReference.cpp
@@ -14,15 +14,26 @@ void ByValueFun(int num1)
     cout << "Number inside function became: " << num1 << endl;
 }
 
+void ByReferenceFun(int& num1) // &: this give us access on the (address) of the variable
+{
+    /*This considered as a (By Reference) function, and unlike the above (By Value) function this 
+    function apply changes on the original variable not on a copy of it.*/
+
+    num1 = 7000;
+
+    cout << "Number inside function became: " << num1 << endl;
+}
+
 int main()
 {
     int num1;
     num1 = 1000;
 
     ByValueFun(num1);
-    cout << "Number after calling the function became: " << num1 << endl;
+    cout << "Number after calling ByValue function became: " << num1 << endl;
 
-
+    ByReferenceFun(num1);
+    cout << "Number after calling ByReference function became: " << num1 << endl;
 
     return 0;
 }
