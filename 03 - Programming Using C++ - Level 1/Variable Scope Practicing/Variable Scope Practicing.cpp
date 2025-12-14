eb4970bc69f02fb0037aa8814cbb9cf99c84f37f
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-14 19:40:37 +0300

    Variable Scope Practicing

--- 03 - Programming Using C++ - Level 1/Variable Scope Practicing/Variable Scope Practicing.cpp
+++ 03 - Programming Using C++ - Level 1/Variable Scope Practicing/Variable Scope Practicing.cpp
@@ -3,19 +3,25 @@
 #include <cmath>
 using namespace std;
 
+int x = 250; // This is a global variable for the whole program
+
 void MyFunction1()
 {
-    int x = 500;
+    int x = 500; // This is a local variable, and its scope inside this function only
 
     cout << "The value of \'x\' inside this function is: " << x << endl;
 }
 
 int main()
 {
-    int x = 100;
+    int x = 100; // This is a local variable, and its scope inside this main function only
 
     cout << "The local value of \'x\' inside this Main function is: " << x << endl;
     MyFunction1();
+
+
+    // To access the global variable, we use this: (::variable):
+    cout << "The global value of \'x\' is: " << ::x << endl;
 
     return 0;
 }
