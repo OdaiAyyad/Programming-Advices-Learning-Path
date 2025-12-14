83a4470295262aaf460a6a80a4d06d72e19be9ba
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-14 22:38:02 +0300

    Reusability

--- 03 - Programming Using C++ - Level 1/Reusability/Reusability.cpp
+++ 03 - Programming Using C++ - Level 1/Reusability/Reusability.cpp
@@ -1,20 +1,38 @@
-// Reusability.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+struct stInfo
+{
+    string FirstName;
+    string LastName;
+    short Age;
+    string Phone;
+};
+
+void ReadInfo(stInfo& Info)
+{
+    cout << "Enter your first name: ";
+    getline(cin, Info.FirstName);
+    cout << "Enter your last name: ";
+    getline(cin, Info.LastName);
+    cout << "Enter your age: ";
+    cin >> Info.Age;
+    cout << "Enter your phone number: ";
+    cin >> Info.Phone;
+}
+
+void PrintInfo(stInfo Info)
+{
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    stInfo Person1Info;
+    ReadInfo(Person1Info);
+    PrintInfo(Person1Info);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
