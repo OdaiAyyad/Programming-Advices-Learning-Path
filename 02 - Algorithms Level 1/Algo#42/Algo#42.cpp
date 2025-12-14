2e7e8818d015581f68237447a3117e313a2300af
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-14 17:40:57 +0300

    Algo#42

--- 02 - Algorithms Level 1/Algo#42/Algo#42.cpp
+++ 02 - Algorithms Level 1/Algo#42/Algo#42.cpp
@@ -2,21 +2,28 @@
 #include <cmath>
 using namespace std;
 
-int main()
+int TotalSeconds(int Days, int Hours, int Minutes, int Seconds)
 {
-    int Days, Hours, Minutes, Seconds, TotalSeconds;
-
-    cout << "Enter number of days: "; cin >> Days;
-    cout << "Enter number of hours: "; cin >> Hours;
-    cout << "Enter number of Minutes: "; cin >> Minutes;
-    cout << "Enter number of seconds: "; cin >> Seconds;
+    int TotalSeconds;
 
     Minutes = Minutes * 60;
     Hours = Hours * 60 * 60;
     Days = Days * 60 * 60 * 24;
     TotalSeconds = Days + Hours + Minutes + Seconds;
 
-    cout << "Your task total time duration in seconds = " << round(TotalSeconds) << endl;
+    return TotalSeconds;
+}
+
+int main()
+{
+    int Days, Hours, Minutes, Seconds;
+
+    cout << "Enter number of days: "; cin >> Days;
+    cout << "Enter number of hours: "; cin >> Hours;
+    cout << "Enter number of Minutes: "; cin >> Minutes;
+    cout << "Enter number of seconds: "; cin >> Seconds;
+
+    cout << "Your task total time duration in seconds = " << round(TotalSeconds(Days, Hours, Minutes, Seconds)) << endl;
 
     return 0;
 }
