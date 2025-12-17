debffbc3e42c43aaae397aaa8c715ad2a66bd7d1
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-17 13:01:37 +0300

    Arrays with Functions

--- 03 - Programming Using C++ - Level 1/Arrays with Functions/Arrays with Functions.cpp
+++ 03 - Programming Using C++ - Level 1/Arrays with Functions/Arrays with Functions.cpp
@@ -1,20 +1,20 @@
-// Arrays with Functions.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+void ReadArrayData(int x[3])
+{
+    cout << "Enter the first number: "; cin >> x[0];
+    cout << "Enter the second number: "; cin >> x[1];
+    cout << "Enter the third number: "; cin >> x[2];
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x[3];
+
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
