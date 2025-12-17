bafc83aa4128308333a0986f52f9d1a60005b4b0
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-17 16:23:03 +0300

    IF and Else Statement

--- 03 - Programming Using C++ - Level 1/IF and IF Else Statement/IF and IF Else Statement.cpp
+++ 03 - Programming Using C++ - Level 1/IF and IF Else Statement/IF and IF Else Statement.cpp
@@ -5,13 +5,28 @@ using namespace std;
 
 int main()
 {
+    // IF Statement
     int x = 10;
 
     if (x >= 5 && x <= 20)
     {
         cout << "The code of IF body has executed.\n";
     }
-    cout << "The code after IF body always executed.\n";
+    cout << "The code after IF body always executed.\n\n\n";
+
+    // IF-Else Statement
+    int z;
+
+    cout << "Enter your number: "; cin >> z;
+
+    if (z > 5)
+    {
+        cout << "Yes, z is greater than 5!\n";
+    }
+    else
+    {
+        cout << "No, z is less that 5.\n";
+    }
 
     return 0;
 }
