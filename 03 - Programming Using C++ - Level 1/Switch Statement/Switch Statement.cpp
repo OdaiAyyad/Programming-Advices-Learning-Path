56e9c887f8ad8f1973ff3b70c9975ad33cdc42b1
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-17 23:35:47 +0300

    Switch Statement

--- 03 - Programming Using C++ - Level 1/Switch Statement/Switch Statement.cpp
+++ 03 - Programming Using C++ - Level 1/Switch Statement/Switch Statement.cpp
@@ -9,7 +9,29 @@ int main()
 
     switch (day)
     {
-
+    case 1:
+        cout << "Sun";
+        break;
+    case 2:
+        cout << "Mon";
+        break;
+    case 3:
+        cout << "Tue";
+        break;
+    case 4:
+        cout << "Wed";
+        break;
+    case 5:
+        cout << "Thu";
+        break;
+    case 6:
+        cout << "Fri";
+        break;
+    case 7:
+        cout << "Sat";
+        break;
+    default:
+        cout << "Not a week day!\n";
     }
 
     return 0;
