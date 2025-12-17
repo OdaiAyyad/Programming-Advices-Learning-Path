7ea09b0c26e3824c2c3523d0a679071fe614b40f
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-17 23:10:38 +0300

    Enums with IF

--- 03 - Programming Using C++ - Level 1/Enums with IF/Enums with IF.cpp
+++ 03 - Programming Using C++ - Level 1/Enums with IF/Enums with IF.cpp
@@ -1,20 +1,47 @@
-// Enums with IF.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+enum enScreenColor { Red = 1, Blue = 2, Green = 3, Yellow = 4 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "*****************\n";
+    cout << "Choose the number of your color: \n";
+    cout << "(1) Red\n";
+    cout << "(2) Blue\n";
+    cout << "(3) Green\n";
+    cout << "(4) Yellow\n";
+    cout << "*****************\n";
+    cout << "Your Choice: ";
+
+    short C;
+    enScreenColor Color;
+
+    cin >> C;
+    Color = enScreenColor(C);
+
+    if (Color == enScreenColor::Red)
+    {
+        system("color 4F");
+    }
+    else if (Color == enScreenColor::Blue)
+    {
+        system("color 1F");
+    }
+    else if (Color == enScreenColor::Green)
+    {
+        system("color 2F");
+    }
+    else if (Color == enScreenColor::Yellow)
+    {
+        system("color 6F");
+    }
+    else
+    {
+        system("color 4F");
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
