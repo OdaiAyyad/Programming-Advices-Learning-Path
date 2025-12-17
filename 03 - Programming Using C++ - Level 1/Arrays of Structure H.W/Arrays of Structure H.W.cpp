c4f56bf640416074dc5bf42e35174c5ea7d0600d
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-17 15:01:07 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Arrays of Structure H.W/Arrays of Structure H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Arrays of Structure H.W/Arrays of Structure H.W.cpp
@@ -11,10 +11,29 @@ struct stInfo
     string Phone;
 };
 
+void ReadPersonInfo(stInfo& Info)
+{
+    cout << "Enter your first name: "; cin >> Info.FirstName;
+    cout << "Enter your last name: "; cin >> Info.LastName;
+    cout << "Enter your age: "; cin >> Info.Age;
+    cout << "Enter your phone number: "; cin >> Info.Phone;
+}
+
+void PrintPersonInfo(stInfo Info)
+{
+    cout << "******************************\n";
+    cout << "First Name: " << Info.FirstName << endl;
+    cout << "Last Name:" << Info.LastName << endl;
+    cout << "Age: " << Info.Age << endl;
+    cout << "Phone Number: " << Info.Phone << "\n\n";
+}
 
 int main()
 {
+    stInfo Person1;
 
+    ReadPersonInfo(Person1);
+    PrintPersonInfo(Person1);
 
     return 0;
 }
