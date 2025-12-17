d6161ff960660a90e80f95b1dde27930a6ecbf64
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-17 20:14:19 +0300

    Algo#36

--- 02 - Algorithms Level 1/Algo#36/Algo#36.cpp
+++ 02 - Algorithms Level 1/Algo#36/Algo#36.cpp
@@ -1,20 +1,43 @@
-// Algo#36.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+void ReadInfo(float &Num1, float &Num2, char &Operation)
+{
+    cout << "Enter the first number: "; cin >> Num1;
+    cout << "Enter the second number: "; cin >> Num2;
+    cout << "Enter the operation you want: "; cin >> Operation;
+}
+
+float Calculator(float Num1, float Num2, char Operation)
+{
+    if (Operation == '+')
+    {
+        return Num1 + Num2;
+    }
+    else if (Operation == '-')
+    {
+        return Num1 - Num2;
+    }
+    else if (Operation == '*')
+    {
+        return Num1 * Num2;
+    }
+    else if (Operation == '/')
+    {
+        return Num1 / Num2;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float Num1, Num2;
+    char Operation;
+
+    ReadInfo(Num1, Num2, Operation);
+
+    cout << Calculator(Num1, Num2, Operation) << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
