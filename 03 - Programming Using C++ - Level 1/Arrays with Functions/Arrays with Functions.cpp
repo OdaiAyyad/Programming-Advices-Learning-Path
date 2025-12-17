019a4e001b8ce94663a0060abbb53b974e83b1fd
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-17 13:07:05 +0300

    Arrays with Functions

--- 03 - Programming Using C++ - Level 1/Arrays with Functions/Arrays with Functions.cpp
+++ 03 - Programming Using C++ - Level 1/Arrays with Functions/Arrays with Functions.cpp
@@ -3,6 +3,9 @@
 #include <cmath>
 using namespace std;
 
+/*Here, we didn't pass the parameter for the function (ByReference(&)), 
+because the parameter here is an array, and the arrays be default passed (ByReference)*/
+
 void ReadArrayData(int x[3])
 {
     cout << "Enter the first number: "; cin >> x[0];
@@ -10,10 +13,16 @@ void ReadArrayData(int x[3])
     cout << "Enter the third number: "; cin >> x[2];
 }
 
+void PrintArrayData(int x[3])
+{
+
+}
+
 int main()
 {
     int x[3];
 
+    ReadArrayData(x);
 
 
     return 0;
