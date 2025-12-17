915c7dae4a574a2692273e9c91cecc9961a70dfb
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-17 14:19:10 +0300

    Arrays of Structure

--- 03 - Programming Using C++ - Level 1/Arrays of Structure/Arrays of Structure.cpp
+++ 03 - Programming Using C++ - Level 1/Arrays of Structure/Arrays of Structure.cpp
@@ -19,7 +19,13 @@ int main()
     People[0].LastName = "Ayyad";
     People[0].Age = 23;
 
+    cout << People[0].FirstName << " " << People[0].LastName << endl;
 
+    People[1].FirstName = "Ali";
+    People[1].LastName = "Mohammad";
+    People[1].Age = 55;
+
+    cout << People[1].FirstName << " " << People[1].LastName << endl;
 
     return 0;
 }
