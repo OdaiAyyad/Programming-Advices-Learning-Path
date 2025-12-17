9de668688d0e5e4830169c6513163ad61f91f6ea
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-17 19:47:10 +0300

    Algo#33

--- 02 - Algorithms Level 1/Algo#33/Algo#33.cpp
+++ 02 - Algorithms Level 1/Algo#33/Algo#33.cpp
@@ -1,20 +1,47 @@
-// Algo#33.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+void ReadGrade(short &Grade)
+{
+    cout << "Enter your grade: "; cin >> Grade;
+}
+
+void GradeRank(short Grade)
+{
+    if (Grade >= 90 && Grade <= 100)
+    {
+        cout << "A\n";
+    }
+    else if (Grade >= 80)
+    {
+        cout << "B\n";
+    }
+    else if (Grade >= 70)
+    {
+        cout << "C\n";
+    }
+    else if (Grade >= 60)
+    {
+        cout << "D\n";
+    }
+    else if (Grade >= 50)
+    {
+        cout << "E\n";
+    }
+    else
+    {
+        cout << "F\n";
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    short Grade;
+
+    ReadGrade(Grade);
+    GradeRank(Grade);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
