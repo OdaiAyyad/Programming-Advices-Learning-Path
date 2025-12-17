da145757bd362f0b8f9a9c3a769ea65720f50b99
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-17 13:21:00 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Arrays with Functions H.W/Arrays with Functions H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Arrays with Functions H.W/Arrays with Functions H.W.cpp
@@ -10,6 +10,14 @@ void ReadGrades(float Grades[3])
     cout << "Enter the third grade: "; cin >> Grades[2];
 }
 
+float AverageCalculation(float Grades[3])
+{
+    float AVG;
+    AVG = (Grades[0] + Grades[1] + Grades[2]) / 3;
+
+    return AVG;
+}
+
 int main()
 {
     float Grades[3];
