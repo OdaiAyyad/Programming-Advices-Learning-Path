56de53a6fcd29e4d4b9059ef0b881e371ff9260e
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-17 20:28:57 +0300

    Algo#44

--- 02 - Algorithms Level 1/Algo#44/Algo#44.cpp
+++ 02 - Algorithms Level 1/Algo#44/Algo#44.cpp
@@ -1,20 +1,52 @@
-// Algo#44.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+void ReadDay(short& Day)
+{
+    cout << "Enter the day number: "; cin >> Day;
+}
+
+string DayText(short Day)
+{
+    if (Day == 1)
+    {
+        return "Sunday";
+    }
+    else if (Day == 2)
+    {
+        return "Monday";
+    }
+    else if (Day == 3)
+    {
+        return "Tuesday";
+    }
+    else if (Day == 4)
+    {
+        return "Wednesday";
+    }
+    else if (Day == 5)
+    {
+        return "Thursday";
+    }
+    else if (Day == 6)
+    {
+        return "Friday";
+    }
+    else if (Day == 7)
+    {
+        return "Satarday";
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    short Day;
+
+    ReadDay(Day);
+
+    cout << DayText(Day);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
