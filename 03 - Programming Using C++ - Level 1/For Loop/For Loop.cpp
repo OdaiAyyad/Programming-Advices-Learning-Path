fd2d26939392e706fcb114e5b499823d7c8e796c
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-18 23:40:49 +0300

    For Loop

--- 03 - Programming Using C++ - Level 1/For Loop/For Loop.cpp
+++ 03 - Programming Using C++ - Level 1/For Loop/For Loop.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+    // 1 - 10
     for (int i = 1; i <= 10; i++)
     {
         cout << i << endl;
@@ -12,11 +13,19 @@ int main()
 
     cout << endl;
 
+    // 10 - 1
     for (int i = 10; i >= 1; i--)
     {
         cout << i << endl;
     }
 
+    cout << endl;
+
+    // Step 2
+    for (int i = 1; i <= 10; i+=2)
+    {
+        cout << i << endl;
+    }
 
     return 0;
 }
