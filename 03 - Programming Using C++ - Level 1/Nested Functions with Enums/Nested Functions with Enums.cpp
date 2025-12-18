5ce1e2f0b5f9978822cd6692efda7ce603e24212
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-18 09:36:09 +0300

    Nested Functions with Enums

--- 03 - Programming Using C++ - Level 1/Nested Functions with Enums/Nested Functions with Enums.cpp
+++ 03 - Programming Using C++ - Level 1/Nested Functions with Enums/Nested Functions with Enums.cpp
@@ -8,7 +8,7 @@ enum enWeekDay { Sun = 1, Mon = 2, Tue = 3, Wed = 4, Thu = 5, Fri = 6, Sat = 7 }
 void WeekDayMenu()
 {
     cout << "*************************\n";
-    cout << "\t\tWeek Days\n";
+    cout << "\tWeek Days\n";
     cout << "*************************\n";
     cout << "1: Sunday\n";
     cout << "2: Monday\n";
@@ -23,17 +23,49 @@ void WeekDayMenu()
 
 enWeekDay ReadWeekDay()
 {
+    enWeekDay WeekDay;
 
+    short  WD;
+    cin >> WD;
+
+    return enWeekDay(WD);
 }
 
 string GetWeekDayName(enWeekDay WeekDay)
 {
-
+    switch (WeekDay)
+    {
+    case enWeekDay::Sun:
+        return "Sunday";
+        break;
+    case enWeekDay::Mon:
+        return "Monday";
+        break;
+    case enWeekDay::Tue:
+        return "Tuesday";
+        break;
+    case enWeekDay::Wed:
+        return "Wednesday";
+        break;
+    case enWeekDay::Thu:
+        return "Thursday";
+        break;
+    case enWeekDay::Fri:
+        return "Friday";
+        break;
+    case enWeekDay::Sat:
+        return "Satarday";
+        break;
+    default:
+        return "Not a Day!";
+    }
 }
 
 int main()
 {
+    WeekDayMenu();
 
+    cout << "Today is: " << GetWeekDayName(ReadWeekDay()) << endl;
 
     return 0;
 }
