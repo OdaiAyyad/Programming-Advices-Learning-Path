568ec5d8a614a53ced615e6c34920e7d7b95f45f
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-18 09:29:27 +0300

    Nested Functions with Enums

--- 03 - Programming Using C++ - Level 1/Nested Functions with Enums/Nested Functions with Enums.cpp
+++ 03 - Programming Using C++ - Level 1/Nested Functions with Enums/Nested Functions with Enums.cpp
@@ -1,20 +1,39 @@
-// Nested Functions with Enums.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+enum enWeekDay { Sun = 1, Mon = 2, Tue = 3, Wed = 4, Thu = 5, Fri = 6, Sat = 7 };
+
+void WeekDayMenu()
+{
+    cout << "*************************\n";
+    cout << "\t\tWeek Days\n";
+    cout << "*************************\n";
+    cout << "1: Sunday\n";
+    cout << "2: Monday\n";
+    cout << "3: Tuesday\n";
+    cout << "4: Wednesday\n";
+    cout << "5: Thursday\n";
+    cout << "6: Friday\n";
+    cout << "7: Satarday\n";
+    cout << "*************************\n";
+    cout << "Enter the number of day you want: ";
+}
+
+enWeekDay ReadWeekDay()
+{
+
+}
+
+string GetWeekDayName(enWeekDay WeekDay)
+{
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
