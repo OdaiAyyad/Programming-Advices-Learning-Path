5b641f6539fa7a53f37e26c80c83f28fa188072c
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-19 11:09:51 +0300

    Algo#27

--- 02 - Algorithms Level 1/Algo#27/Algo#27.cpp
+++ 02 - Algorithms Level 1/Algo#27/Algo#27.cpp
@@ -3,16 +3,21 @@
 #include <cmath>
 using namespace std;
 
+void PrintNumbers(short Num)
+{
+    for (int i = Num; i >= 1; i--)
+    {
+        cout << i << endl;
+    }
+}
+
 int main()
 {
     short Num;
 
     cout << "Enter a number: "; cin >> Num;
 
-    for (int i = Num; i >= 1; i--)
-    {
-        cout << i << endl;
-    }
+    PrintNumbers(Num);
 
     return 0;
 }
