22606eebbf9fabe9cafd0d2fa1b20b164f12acde
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-19 18:50:38 +0300

    While Loop

---
+++ 03 - Programming Using C++ - Level 1/While Loop/While Loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+// We always use (while loops) when we didn't know the ranges of the task (from what to what)
+
+int main()
+{
+    int i = 1;
+    while (i <= 5)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
