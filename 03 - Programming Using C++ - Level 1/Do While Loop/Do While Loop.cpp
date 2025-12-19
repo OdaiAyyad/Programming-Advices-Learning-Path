c4760e273e31e7f4807486906e7f911e4280d449
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-19 22:21:04 +0300

    Do While Loop

--- 03 - Programming Using C++ - Level 1/Do While Loop/Do While Loop.cpp
+++ 03 - Programming Using C++ - Level 1/Do While Loop/Do While Loop.cpp
@@ -3,9 +3,17 @@
 #include <cmath>
 using namespace std;
 
+/*This (do while) loop is the same as the normal while loop, 
+but in this loop the body executed at least one time before checking the loop body*/
+
 int main()
 {
-
+    int i = 6;
+    do
+    {
+        cout << "Odai\n";
+        i++;
+    } while (i <= 5);
 
     return 0;
 }
