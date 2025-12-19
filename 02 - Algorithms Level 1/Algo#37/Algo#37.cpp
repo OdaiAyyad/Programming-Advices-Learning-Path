a2d8a9517366bb1b2985056ccfcb6e86fdbb7b7f
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-19 21:08:24 +0300

    Algo#37

--- 02 - Algorithms Level 1/Algo#37/Algo#37.cpp
+++ 02 - Algorithms Level 1/Algo#37/Algo#37.cpp
@@ -5,14 +5,16 @@ using namespace std;
 
 int main()
 {
-    int Num;
+    int Num, Sum = 0;
 
     cout << "Enter a number: "; cin >> Num;
 
     while (Num != -99)
     {
         cout << "Enter a number: "; cin >> Num;
+        Sum += Num;
     }
+    cout << "Summation = " << Sum << endl;
 
     return 0;
 }
