6da387d6cf8983de5f432a9378a7690aca987ff5
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-19 20:12:05 +0300

    Algo#26

--- 02 - Algorithms Level 1/Algo#26/Algo#26.cpp
+++ 02 - Algorithms Level 1/Algo#26/Algo#26.cpp
@@ -26,13 +26,11 @@ int main()
     short Num1, Num2;
 
     cout << "Enter a number: "; cin >> Num1;
-
     PrintNumbersForLoop(Num1);
 
     cout << "\n\n";
 
     cout << "Enter a number: "; cin >> Num2;
-
     PrintNumbersWhileLoop(Num2);
 
     return 0;
