7355637e70497567b66e024a7ff62b51fefe74a0
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-19 22:32:23 +0300

    Do While Loop

--- 03 - Programming Using C++ - Level 1/Do While Loop/Do While Loop.cpp
+++ 03 - Programming Using C++ - Level 1/Do While Loop/Do While Loop.cpp
@@ -6,6 +6,18 @@ using namespace std;
 /*This (do while) loop is the same as the normal while loop, 
 but in this loop the body executed at least one time before checking the loop body*/
 
+int ReadNumberInRange(int From, int To)
+{
+    int Number;
+
+    do
+    {
+        cout << "Enter a number between " << From << " and " << To << ": "; cin >> Number;
+    } while (Number < From || Number > To);
+
+    return Number;
+}
+
 int main()
 {
     int i = 6;
@@ -14,6 +26,8 @@ int main()
         cout << "Odai\n";
         i++;
     } while (i <= 5);
+
+    cout << "\nThe Number is: " << ReadNumberInRange(18, 45) << endl;
 
     return 0;
 }
