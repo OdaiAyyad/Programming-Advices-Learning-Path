cd00d09f28889fd06e4bdd47a83e73ff8bcab608
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-19 19:54:36 +0300

    While Loop

--- 03 - Programming Using C++ - Level 1/While Loop/While Loop.cpp
+++ 03 - Programming Using C++ - Level 1/While Loop/While Loop.cpp
@@ -5,6 +5,20 @@ using namespace std;
 
 // We always use (while loops) when we (didn't know) the ranges of the task (from what to what)
 
+int ReadNumberInRange(int From, int To)
+{
+    int Number;
+
+    cout << "Enter a number between " << From << " and " << To << ": "; cin >> Number;
+
+    while (Number < From || Number > To)
+    {
+        cout << "Wrong Number!\n";
+        cout << "Enter a number between " << From << " and " << To << ": "; cin >> Number;
+    }
+    return Number;
+}
+
 int main()
 {
     // Print form 1 to 5
@@ -27,6 +41,11 @@ int main()
         cout << "Wrong number, please enter a positive number: "; cin >> Number;
     }
     cout << "\nThe number you entered is: " << Number << endl;
+
+    cout << "\n\n";
+
+    // Read number between
+    cout << "The Number is: " << ReadNumberInRange(18, 45);
 
     return 0;
 }
