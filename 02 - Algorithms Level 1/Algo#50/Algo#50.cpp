263834ad403167eab91969130ef8146383463b23
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-19 21:50:18 +0300

    Algo#50

--- 02 - Algorithms Level 1/Algo#50/Algo#50.cpp
+++ 02 - Algorithms Level 1/Algo#50/Algo#50.cpp
@@ -1,20 +1,24 @@
-// Algo#50.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string PIN;  int Balance = 7500; int Counter = 0;
+
+    cout << "Enter your PIN: "; cin >> PIN;
+
+    while (PIN != "1234")
+    {
+        cout << "\nWrong PIN!\n";
+        cout << "Enter your PIN: "; cin >> PIN;
+        Counter++;
+        if (Counter == 3)
+        {
+            cout << "Card is locked!\n";
+        }
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
