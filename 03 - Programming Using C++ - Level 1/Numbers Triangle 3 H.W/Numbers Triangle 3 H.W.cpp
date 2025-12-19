def6d9528933e5dd4243b8096c6002639948da5b
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-19 18:45:21 +0300

    H.W

--- 03 - Programming Using C++ - Level 1/Numbers Triangle 3 H.W/Numbers Triangle 3 H.W.cpp
+++ 03 - Programming Using C++ - Level 1/Numbers Triangle 3 H.W/Numbers Triangle 3 H.W.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main()
 {
-    for (int i = 10; i >= 1; i--)
+    for (int i = 1; i <= 10; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = i; j <= 10; j++)
         {
             cout << j << " ";
         }
