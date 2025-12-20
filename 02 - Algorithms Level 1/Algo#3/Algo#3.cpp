0a62917789dcfb33b702646a4bf474d286ab409f
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-20 13:28:57 +0300

    Algo#3

--- 02 - Algorithms Level 1/Algo#3/Algo#3.cpp
+++ 02 - Algorithms Level 1/Algo#3/Algo#3.cpp
@@ -3,40 +3,35 @@
 #include <cmath>
 using namespace std;
 
-enum enNumberType { Odd = 1, Even = 0};
+enum enNumberType { Odd = 1, Even = 2 };
 
 int ReadNumber()
 {
     int Number;
-
     cout << "Enter a number: "; cin >> Number;
 
     return Number;
 }
 
-enNumberType CheckOddEven(int Number)
+enNumberType CeckNumberType(int Number)
 {
     if (Number % 2 == 0)
-    {
         return enNumberType::Even;
-    }
     else
-    {
         return enNumberType::Odd;
-    }
 }
 
-void PrintNumberType(enNumberType NumberType)
+void PrintNumber(enNumberType NumberType)
 {
     if (NumberType == enNumberType::Even)
-        cout << "\nThe Number is Even!\n";
+        cout << "This is an Even Number\n";
     else
-        cout << "\nThe Number is Odd!\n";
+        cout << "This is an Odd Number\n";
 }
 
 int main()
 {
-    PrintNumberType(CheckOddEven(ReadNumber()));
+    PrintNumber(CeckNumberType(ReadNumber()));
 
     return 0;
 }
