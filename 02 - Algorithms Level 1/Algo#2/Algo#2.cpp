9f55551812aa598cc8c9a592e6651bb2405d2e36
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-20 10:31:33 +0300

    Algo#2

--- 02 - Algorithms Level 1/Algo#2/Algo#2.cpp
+++ 02 - Algorithms Level 1/Algo#2/Algo#2.cpp
@@ -3,19 +3,24 @@
 #include <cmath>
 using namespace std;
 
+string ReadName()
+{
+    string Name;
+
+    cout << "Enter your name: ";
+    getline(cin, Name);
+
+    return Name;
+}
+
 void PrintName(string Name)
 {
-    cout << Name << endl;
+    cout << "\nYour name is: " << Name << endl;
 }
 
 int main()
 {
-    string Name;
-
-    cout << "Enter your name: "; 
-    getline(cin, Name);
-
-    PrintName(Name);
+    PrintName(ReadName());
 
     return 0;
 }
