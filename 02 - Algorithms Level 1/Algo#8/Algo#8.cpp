be920c1a7b2a43c6752972a9be60d4fc0c16f8bb
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-20 21:00:46 +0300

    Algo#8

--- 02 - Algorithms Level 1/Algo#8/Algo#8.cpp
+++ 02 - Algorithms Level 1/Algo#8/Algo#8.cpp
@@ -3,20 +3,33 @@
 #include <cmath>
 using namespace std;
 
-int main()
+int ReadMark()
 {
     short Mark;
-
     cout << "Enter your mark: "; cin >> Mark;
 
+    return Mark;
+}
+
+bool CheckMark(short Mark)
+{
     if (Mark >= 50)
-    {
-        cout << "PASS!\n";
-    }
+        return true;
     else
-    {
+        return false;
+}
+
+void PrintResult(short Mark)
+{
+    if(CheckMark(Mark) == true)
+        cout << "PASS!\n";
+    else
         cout << "Fail.\n";
-    }
+}
+
+int main()
+{
+    PrintResult(ReadMark());
 
     return 0;
 }
