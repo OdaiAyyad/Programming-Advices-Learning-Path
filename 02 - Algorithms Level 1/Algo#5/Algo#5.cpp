eeaa141f0f10494d82416eda4c6ade14b9f3fae4
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-20 15:30:31 +0300

    Algo#5

--- 02 - Algorithms Level 1/Algo#5/Algo#5.cpp
+++ 02 - Algorithms Level 1/Algo#5/Algo#5.cpp
@@ -1,20 +1,45 @@
-// Algo#5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+struct stInfo
+{
+    short Age;
+    bool HasDriverLicense;
+    bool HasRecommendation;
+};
+
+stInfo ReadInfo()
+{
+    stInfo Info;
+
+    cout << "Enter your age: "; cin >> Info.Age;
+    cout << "Do you have a driver license? "; cin >> Info.HasDriverLicense;
+    cout << "Do you have a recommendation? "; cin >> Info.HasRecommendation;
+
+    return Info;
+}
+
+bool IsAccepted(stInfo Info)
+{
+    if (Info.HasRecommendation == true)
+        return true;
+    else
+        return (Info.Age > 21 && Info.HasDriverLicense == true);
+}
+
+void PrintInfo(stInfo Info)
+{
+    if (IsAccepted(Info))
+        cout << "\nHired!\n";
+    else
+        cout << "\nRejected!\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    PrintInfo(ReadInfo());
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
