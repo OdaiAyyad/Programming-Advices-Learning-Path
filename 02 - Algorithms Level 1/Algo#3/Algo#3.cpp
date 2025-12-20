95bd30c43a6d398fba48ee623e7ebdf34bdd89c5
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-20 10:46:28 +0300

    Algo#3

--- 02 - Algorithms Level 1/Algo#3/Algo#3.cpp
+++ 02 - Algorithms Level 1/Algo#3/Algo#3.cpp
@@ -1,20 +1,42 @@
-// Algo#3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+enum enNumberType { Odd = 1, Even = 0};
+
+int ReadNumber()
+{
+    int Number;
+
+    cout << "Enter a number: "; cin >> Number;
+
+    return Number;
+}
+
+enNumberType CheckOddEven(int Number)
+{
+    if (Number % 2 == 0)
+    {
+        return enNumberType::Even;
+    }
+    else
+    {
+        return enNumberType::Odd;
+    }
+}
+
+void PrintNumberType(enNumberType NumberType)
+{
+    if (NumberType == enNumberType::Even)
+        cout << "\nThe Number is Even!\n";
+    else
+        cout << "\nThe Number is Odd!\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    PrintNumberType(CheckOddEven(ReadNumber()));
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
