76ef6ffb5087b950115ea9bd6d874ba2a7c1e02a
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-20 21:32:57 +0300

    Algo#9

--- 02 - Algorithms Level 1/Algo#9/Algo#9.cpp
+++ 02 - Algorithms Level 1/Algo#9/Algo#9.cpp
@@ -1,15 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main()
+struct stNumbers
 {
     int num1, num2, num3;
+};
 
-    cout << "Enter the first number: "; cin >> num1;
-    cout << "Enter the second number: "; cin >> num2;
-    cout << "Enter the third number: "; cin >> num3;
+stNumbers ReadNumbers()
+{
+    stNumbers Numbers;
 
-    cout << "Total = " << num1 + num2 + num3 << endl;
+    cout << "Enter the first number: "; cin >> Numbers.num1;
+    cout << "Enter the second number: "; cin >> Numbers.num2;
+    cout << "Enter the third number: "; cin >> Numbers.num3;
+
+    return Numbers;
+}
+
+int CalculateSummation(stNumbers Numbers)
+{
+    return Numbers.num1 + Numbers.num2 + Numbers.num3;
+}
+
+void PrintResult(int Sum)
+{
+    cout << "Total = " << Sum << endl;
+}
+
+int main()
+{
+    PrintResult(CalculateSummation(ReadNumbers()));
 
     return 0;
 }
