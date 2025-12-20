7b4f2c4bd56ad72d224cb114a6d5ad01cfedde4f
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-20 17:28:21 +0300

    Algo#6

--- 02 - Algorithms Level 1/Algo#6/Algo#6.cpp
+++ 02 - Algorithms Level 1/Algo#6/Algo#6.cpp
@@ -3,9 +3,29 @@
 #include <cmath>
 using namespace std;
 
+struct stFullName
+{
+    string FisrtName, LastName;
+};
+
+stFullName ReadFullName()
+{
+    stFullName Name;
+
+    cout << "Enter your first name: "; cin >> Name.FisrtName;
+    cout << "Enter your last name: "; cin >> Name.LastName;
+
+    return Name;
+}
+
+void PrintFullName(stFullName Name)
+{
+    cout << "\nFull Name: " << Name.FisrtName << " " << Name.LastName << "\n\n";
+}
+
 int main()
 {
-
+    PrintFullName(ReadFullName());
 
     return 0;
 }
