7b1914dda2e37948c0c7abf492965d637b9bf7a4
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-25 20:18:40 +0300

    Algo#12

--- 02 - Algorithms Level 1/Algo#12/Algo#12.cpp
+++ 02 - Algorithms Level 1/Algo#12/Algo#12.cpp
@@ -3,9 +3,33 @@
 #include <cmath>
 using namespace std;
 
+void ReadNumbers(int& Num1, int& Num2)
+{
+    cout << "Enter the first number: "; cin >> Num1;
+    cout << "Enter the second number: "; cin >> Num2;
+}
+
+int MaxOfTwoNumbers(int Num1, int Num2)
+{
+    int Max = 0;
+    if (Num1 > Num2)
+        Max = Num1;
+    else
+        Max = Num2;
+
+    return Max;
+}
+
+void PrintResults(int Max)
+{
+    cout << "The max number is: " << Max << endl;
+}
+
 int main()
 {
+    int Num1, Num2;
 
+    PrintResults(MaxOfTwoNumbers(Num1, Num2));
 
     return 0;
 }
