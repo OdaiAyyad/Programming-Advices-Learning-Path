d0e492dcea854c2333d73a0ec7dc7ea97348d661
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-25 19:53:51 +0300

    Algo#11

--- 02 - Algorithms Level 1/Algo#11/Algo#11.cpp
+++ 02 - Algorithms Level 1/Algo#11/Algo#11.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 using namespace std;
 
+enum enPassFail { Pass = 1, Fail = 2};
+
 void ReadMarks(float Marks[3])
 {
     cout << "Enter the first mark: "; cin >> Marks[0];
@@ -10,9 +12,29 @@ void ReadMarks(float Marks[3])
     cout << "Enter the third mark: "; cin >> Marks[2];
 }
 
+int MarksSummation(float Marks[3])
+{
+    return Marks[0] + Marks[1] + Marks[2];
+}
+
 float AverageCalculator(float Marks[3])
 {
-    return (Marks[0] + Marks[1] + Marks[2]) / 3;
+    return float((MarksSummation(Marks) / 3));
+}
+
+enPassFail CheckAverage(float Average)
+{
+    if()
+}
+
+void PrintResults(float Average)
+{
+    cout << "\nAverage = " << Average << endl;
+
+    if (CheckAverage(Average) == enPassFail::Pass)
+        cout << "\nYou Passed!\n";
+    else
+        cout << "\nYou Failed!\n";
 }
 
 int main()
