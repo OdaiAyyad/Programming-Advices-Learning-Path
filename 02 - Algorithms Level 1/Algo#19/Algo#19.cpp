1f291efcf853c257525e91872f5cc3012a27ba2b
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-25 21:35:27 +0300

    Algo#19

--- 02 - Algorithms Level 1/Algo#19/Algo#19.cpp
+++ 02 - Algorithms Level 1/Algo#19/Algo#19.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-float Area(float D)
+float ReadDiameter()
+{
+    float D;
+    cout << "Enter the circle diameter: "; cin >> D;
+
+    return D;
+}
+
+float CalculateArea(float D)
 {
     const float PI = 3.14;
     float Area = PI * pow(D, 2) / 4;
@@ -9,13 +17,14 @@ float Area(float D)
     return Area;
 }
 
+void PrintArea(float Area)
+{
+    cout << "\nArea = " << ceil(Area) << endl;
+}
+
 int main()
 {
-    unsigned int D;
-
-    cout << "Enter the circle diameter: "; cin >> D;
-
-    cout << "\nArea = " << ceil(Area(D)) << endl;
+    PrintArea(CalculateArea(ReadDiameter()));
 
     return 0;
 }
