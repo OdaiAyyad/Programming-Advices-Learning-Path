fa1e1b8096f92044e5c091a49f4a303b7e2a80b6
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-25 20:38:27 +0300

    Algo#14

--- 02 - Algorithms Level 1/Algo#14/Algo#14.cpp
+++ 02 - Algorithms Level 1/Algo#14/Algo#14.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void ReadNumbers(int& num1, int& num2)
+{
+    cout << "Enter the first number: "; cin >> num1;
+    cout << "Enter the second number: "; cin >> num2;
+}
+
 void Swap(int &num1, int &num2)
 {
     int temp;
@@ -8,7 +14,10 @@ void Swap(int &num1, int &num2)
     temp = num2;
     num2 = num1;
     num1 = temp;
+}
 
+void PrintNumbers(int num1, int num2)
+{
     cout << "Num1 = " << num1 << endl;
     cout << "Num2 = " << num2 << endl;
 }
@@ -17,14 +26,14 @@ int main()
 {
     int num1, num2;
 
-    cout << "Enter the first number: "; cin >> num1;
-    cout << "Enter the second number: "; cin >> num2;
+    ReadNumbers(num1, num2);
+    cout << "\nNumbers Before Swapping:\n";
+    PrintNumbers(num1, num2);
 
-    cout << "Num1 = " << num1 << endl;
-    cout << "Num2 = " << num2 << endl;
 
-    cout << "\nAfter Swapping:\n";
+    cout << "\nNumbers After Swapping:\n";
     Swap(num1, num2);
+    PrintNumbers(num1, num2);
 
     return 0;
 }
