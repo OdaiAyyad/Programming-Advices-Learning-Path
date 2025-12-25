f88cd07bbe2e45287559959055edd8f5ac12cc27
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-25 20:21:16 +0300

    Algo#13

--- 02 - Algorithms Level 1/Algo#13/Algo#13.cpp
+++ 02 - Algorithms Level 1/Algo#13/Algo#13.cpp
@@ -1,20 +1,36 @@
-// Algo#13.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+void ReadNumbers(int& Num1, int& Num2)
+{
+    cout << "Enter the first number: "; cin >> Num1;
+    cout << "Enter the second number: "; cin >> Num2;
+}
+
+int MaxOfTwoNumbers(int Num1, int Num2)
+{
+    int Max = 0;
+    if (Num1 > Num2)
+        Max = Num1;
+    else
+        Max = Num2;
+
+    return Max;
+}
+
+void PrintResults(int Max)
+{
+    cout << "\nThe max number is: " << Max << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int Num1, Num2;
+
+    ReadNumbers(Num1, Num2);
+    PrintResults(MaxOfTwoNumbers(Num1, Num2));
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
