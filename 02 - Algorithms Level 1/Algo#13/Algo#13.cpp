c66f20235a131bf8a2b2fc2fd41d490700f3bae2
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-25 20:23:41 +0300

    Algo#13

--- 02 - Algorithms Level 1/Algo#13/Algo#13.cpp
+++ 02 - Algorithms Level 1/Algo#13/Algo#13.cpp
@@ -10,7 +10,7 @@ void ReadNumbers(int& Num1, int& Num2, int& Num3)
     cout << "Enter the third number: "; cin >> Num3;
 }
 
-int MaxOfTwoNumbers(int Num1, int Num2)
+int MaxOfTwoNumbers(int Num1, int Num2, int Num3)
 {
     int Max = 0;
     if (Num1 > Num2)
@@ -28,10 +28,10 @@ void PrintResults(int Max)
 
 int main()
 {
-    int Num1, Num2;
+    int Num1, Num2, Num3;
 
-    ReadNumbers(Num1, Num2);
-    PrintResults(MaxOfTwoNumbers(Num1, Num2));
+    ReadNumbers(Num1, Num2, Num3);
+    PrintResults(MaxOfTwoNumbers(Num1, Num2, Num3));
 
     return 0;
 }
