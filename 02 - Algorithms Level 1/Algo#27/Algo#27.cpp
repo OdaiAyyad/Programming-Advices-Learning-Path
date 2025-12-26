e56a825852b41eaaa8ab49975c4d74814ac0fa56
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-26 23:22:04 +0300

    Algo#27

--- 02 - Algorithms Level 1/Algo#27/Algo#27.cpp
+++ 02 - Algorithms Level 1/Algo#27/Algo#27.cpp
@@ -3,35 +3,29 @@
 #include <cmath>
 using namespace std;
 
+int ReadNumber()
+{
+    int N;
+    cout << "Enter a number: "; cin >> N;
+
+    return N;
+}
+
 void PrintNumbersForLoop(short Num)
 {
-    for (int i = Num; i >= 1; i--)
-    {
-        cout << i << endl;
-    }
+
 }
 
 void PrintNumbersWhileLoop(short Num)
 {
-    int i = Num;
-    while (i >= 1)
-    {
-        cout << i << endl;
-        i--;
-    }
+
 }
 
 int main()
 {
-    short Num1, Num2;
+    int N = ReadNumber();
 
-    cout << "Enter a number: "; cin >> Num1;
-    PrintNumbersForLoop(Num1);
 
-    cout << "\n\n";
-
-    cout << "Enter a number: "; cin >> Num2;
-    PrintNumbersWhileLoop(Num2);
 
     return 0;
 }
