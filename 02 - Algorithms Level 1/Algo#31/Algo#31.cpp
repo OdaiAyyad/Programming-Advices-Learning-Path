0df5e2f2fd7f083b4367a399eb401a4b18299fed
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-27 23:22:49 +0300

    Algo#31

--- 02 - Algorithms Level 1/Algo#31/Algo#31.cpp
+++ 02 - Algorithms Level 1/Algo#31/Algo#31.cpp
@@ -2,26 +2,19 @@
 #include <cmath>
 using namespace std;
 
-int Power_2(int N)
+int ReadNumber()
 {
-    return pow(N, 2);
+    int N;
+    cout << "Enter a Number: "; cin >> N;
+
+    return N;
 }
 
-int Power_3(int N)
-{
-    return pow(N, 3);
-}
 
-int Power_4(int N)
-{
-    return pow(N, 4);
-}
 
 int main()
 {
-    int N;
 
-    cout << "Enter a Number: "; cin >> N;
 
     cout << N << " ^ 2" << " = " << round(Power_2(N)) << endl;
     cout << N << " ^ 3" << " = " << round(Power_3(N)) << endl;
