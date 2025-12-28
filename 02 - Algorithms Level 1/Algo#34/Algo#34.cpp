063e208e4aa116a44f5300305b0ace83bf4eba89
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-28 13:30:59 +0300

    Algo#34

--- 02 - Algorithms Level 1/Algo#34/Algo#34.cpp
+++ 02 - Algorithms Level 1/Algo#34/Algo#34.cpp
@@ -27,24 +27,15 @@ float GetCommissionPercentage(float TotalSales)
 
 float CalculateTotalCommission(float TotalSales)
 {
-    if (TotalSales >= 1000000)
-        return (TotalSales * 0.01);
-    else if (TotalSales >= 500000)
-        return (TotalSales * 0.02);
-    else if (TotalSales >= 100000)
-        return (TotalSales * 0.03);
-    else if (TotalSales >= 50000)
-        return (TotalSales * 0.05);
-    else
-        return 0;
+    return GetCommissionPercentage(TotalSales) * TotalSales;
 }
 
 int main()
 {
     float TotalSales = ReadTotalSales();
 
-    cout << "\nYour commission percentage is: " << GetCommissionPercentage(TotalSales) << endl;
-    cout << "\nYour total commission is: " << CalculateTotalCommission(TotalSales) << endl;
+    cout << "\nYour commission percentage = " << GetCommissionPercentage(TotalSales) << endl;
+    cout << "\nYour total commission = " << CalculateTotalCommission(TotalSales) << endl;
 
     return 0;
 }
