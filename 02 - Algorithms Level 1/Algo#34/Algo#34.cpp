652e8c166e096a7911b5a029496786d9519319c5
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-28 12:50:25 +0300

    Algo#34

--- 02 - Algorithms Level 1/Algo#34/Algo#34.cpp
+++ 02 - Algorithms Level 1/Algo#34/Algo#34.cpp
@@ -11,26 +11,15 @@ void ReadTotalSales(float &TotalSales)
 float Commission(float TotalSales)
 {
     if (TotalSales >= 1000000)
-    {
         return (TotalSales * 0.01);
-    }
     else if (TotalSales >= 500000)
-    {
         return (TotalSales * 0.02);
-    }
     else if (TotalSales >= 100000)
-    {
         return (TotalSales * 0.03);
-    }
     else if (TotalSales >= 50000)
-    {
         return (TotalSales * 0.05);
-    }
     else
-    {
         return 0;
-    }
-
 }
 
 int main()
