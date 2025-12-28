02c2c11becf4c77475d9319036332e3f732bf83b
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-28 13:52:12 +0300

    Algo#35

--- 02 - Algorithms Level 1/Algo#35/Algo#35.cpp
+++ 02 - Algorithms Level 1/Algo#35/Algo#35.cpp
@@ -6,15 +6,24 @@ struct stPiggyBankContent
     unsigned int Pennies, Nickels, Dimes, Quarters, Dollars;
 };
 
+stPiggyBankContent ReadPiggyBankContent()
+{
+    stPiggyBankContent Content;
+
+    cout << "Enter the number of Pennies: "; cin >> Content.Pennies;
+    cout << "Enter the number of Nickels: "; cin >> Content.Nickels;
+    cout << "Enter the number of Dimes: "; cin >> Content.Dimes;
+    cout << "Enter the number of Quarters: "; cin >> Content.Quarters;
+    cout << "Enter the number of Dollars: "; cin >> Content.Dollars;
+
+    return Content;
+}
+
 int main()
 {
     unsigned int Pennies, Nickels, Dimes, Quarters, Dollars;
 
-    cout << "Enter the number of Pennies: "; cin >> Pennies;
-    cout << "Enter the number of Nickels: "; cin >> Nickels;
-    cout << "Enter the number of Dimes: "; cin >> Dimes;
-    cout << "Enter the number of Quarters: "; cin >> Quarters;
-    cout << "Enter the number of Dollars: "; cin >> Dollars;
+
 
     float TotalPennies = Pennies + 5 * Nickels + 10 * Dimes + 25 * Quarters + 100 * Dollars;
 
