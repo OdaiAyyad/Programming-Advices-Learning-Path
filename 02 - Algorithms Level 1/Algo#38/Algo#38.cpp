19b7bd4bbaa718c3c4c939149d43652f92b9e00e
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-28 15:08:28 +0300

    Algo#38

--- 02 - Algorithms Level 1/Algo#38/Algo#38.cpp
+++ 02 - Algorithms Level 1/Algo#38/Algo#38.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 using namespace std;
 
+enum enPrime_NotPrime { Prime = 1, NotPrime = 2 };
+
 int ReadNumber(string Message)
 {
     int Number;
@@ -11,9 +13,28 @@ int ReadNumber(string Message)
     return Number;
 }
 
+enPrime_NotPrime CheckPrime_NotPrime(int Number)
+{
+    for (int i = 2; i <= Number; i++)
+    {
+        if (Number % i == 0)
+            return enPrime_NotPrime::NotPrime;
+        else
+            return enPrime_NotPrime::Prime;
+    }
+}
+
+void PrintResults(enPrime_NotPrime P)
+{
+    if (enPrime_NotPrime(P) == enPrime_NotPrime::Prime)
+        cout << "\nPrime Number.\n";
+    else
+        cout << "\nNot a Prime Number!!\n";
+}
+
 int main()
 {
-
+    PrintResults(CheckPrime_NotPrime(ReadNumber("Enter a number: ")));
 
     return 0;
 }
