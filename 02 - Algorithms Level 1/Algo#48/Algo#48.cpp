b68184b0462987ec372dc783a91951217862ca82
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-28 19:59:55 +0300

    Algo#48

--- 02 - Algorithms Level 1/Algo#48/Algo#48.cpp
+++ 02 - Algorithms Level 1/Algo#48/Algo#48.cpp
@@ -13,14 +13,19 @@ float ReadPositiveNumber(string Message)
     return Number;
 }
 
+float MonthlyPayment(float LoanAmount, float NumberOfMonths)
+{
+    float MonthlyPayment = LoanAmount / NumberOfMonths;
+
+    return MonthlyPayment;
+}
+
 int main()
 {
     int LoanAmount, Months;
 
     cout << "Enter your loan amount: "; cin >> LoanAmount;
     cout << "How many months you need to settle this loan? "; cin >> Months;
-
-    int MonthlyPyment = LoanAmount / Months;
 
     cout << "\nYour monthly payment will be " << MonthlyPyment << endl
 
