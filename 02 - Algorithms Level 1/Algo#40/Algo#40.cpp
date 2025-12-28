4c5ef3143da977a77e56aa544d6210e90ede4768
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-28 15:33:11 +0300

    Algo#40

--- 02 - Algorithms Level 1/Algo#40/Algo#40.cpp
+++ 02 - Algorithms Level 1/Algo#40/Algo#40.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main()
+float ReadPositiveNumber(string Message)
 {
-    float BillValue, TotalBill;
+    float Number;
+    do
+    {
+        cout << Message; cin >> Number;
 
-    cout << "Enter your bill value: "; cin >> BillValue;
+    } while (Number < 0);
+
+    return Number;
+}
+
+float CalculateTotalBillAfterServiceAndTax(float BillValue)
+{
+    float TotalBill;
 
     TotalBill = BillValue * 1.10;
     TotalBill = TotalBill * 1.16;
 
-    cout << "Your total bill = " << TotalBill << endl;
+    return TotalBill;
+}
+
+int main()
+{
+    float BillValue = ReadPositiveNumber("Enter your bill value: ");
+
+    cout << "\n";
+    cout << "Bill Value = " << BillValue << endl;
+    cout << "Total Bill after Service Fee and Sales Tax = " << CalculateTotalBillAfterServiceAndTax(BillValue) << endl;
 
     return 0;
 }
