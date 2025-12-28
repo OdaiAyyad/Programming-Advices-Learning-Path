3bc0a4644289fbdd5b3e65eca6d65df2eeb124e5
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-28 19:49:03 +0300

    Algo#46

--- 02 - Algorithms Level 1/Algo#46/Algo#46.cpp
+++ 02 - Algorithms Level 1/Algo#46/Algo#46.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std;
 
-void PrintNumbersForLoop()
+void PrintLettersFromAtoZ()
 {
     for (int i = 65; i <= 90; i++)
     {
@@ -11,21 +11,9 @@ void PrintNumbersForLoop()
     }
 }
 
-void PrintNumbersWhileLoop()
-{
-    int i = 65;
-    while (i <= 90)
-    {
-        cout << char(i) << endl;
-        i++;
-    }
-}
-
 int main()
 {
-    PrintNumbersForLoop();
-    cout << "\n-----\n\n";
-    PrintNumbersWhileLoop();
+    PrintLettersFromAtoZ();
 
     return 0;
 }
