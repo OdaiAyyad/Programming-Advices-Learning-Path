14ffbaba57fc651370f2dd5bc933c4a0821ff0b8
Odai Ayyad <3odaysuheel10@gmail.com>
2025-12-28 14:57:29 +0300

    Algo#37

--- 02 - Algorithms Level 1/Algo#37/Algo#37.cpp
+++ 02 - Algorithms Level 1/Algo#37/Algo#37.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std;
 
-int ReadNumber(string Message)
+float ReadNumber(string Message)
 {
     int N;
     cout << Message; cin >> N;
@@ -11,18 +11,27 @@ int ReadNumber(string Message)
     return N;
 }
 
-int main()
+float Summation()
 {
+    int N = 0;
     int Sum = 0;
 
-    int N = ReadNumber("Enter a number : ");
-
-    while (Num != -99)
+    do
     {
-        cout << "Enter a number: "; cin >> Num;
-        Sum += Num;
-    }
-    cout << "\nSummation = " << Sum << endl
+        N = ReadNumber("Enter a number : ");
+        if (N == -99)
+            break;
+
+        Sum += N;
+
+    } while (N != -99);
+
+    return Sum;
+}
+
+int main()
+{
+    cout << "\nSummation = " << Summation() << endl;
 
     return 0;
 }
