3d633a364695a21b4c0b4f60a7fe22eb2f6f54e1
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-01 19:13:07 +0300

    Algo#1

--- 05 - Algorithms & Problem-Solving – Level 2/Algo#1/Algo#1.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#1/Algo#1.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std;
 
-void PrintHeader()
+void PrintTableHeader()
 {
     cout << "\t\t\tMultiplication Table From 1 to 10\n\n";
     cout << "\t  ";
@@ -30,7 +30,7 @@ void PrintTable()
 
 int main()
 {
-    PrintHeader();
+    PrintTableHeader();
     PrintTable();
 
     return 0;
