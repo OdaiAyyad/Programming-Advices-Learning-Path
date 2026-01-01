94842cdc6266c51931453db0eaccc7b5ec04fb44
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-01 20:41:40 +0300

    Algo#3

--- 05 - Algorithms & Problem-Solving – Level 2/Algo#3/Algo#3.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#3/Algo#3.cpp
@@ -3,8 +3,6 @@
 #include <cmath>
 using namespace std;
 
-enum enPerfect_NotPerfect { Perfect = 1, Not_Perfect = 2 };
-
 short ReadPositiveNumber(string Message)
 {
     short Number;
@@ -17,25 +15,21 @@ short ReadPositiveNumber(string Message)
     return Number;
 }
 
-enPerfect_NotPerfect CheckPerfectNumber(short Number)
+bool IsPerfectNumber(short Number)
 {
-    short M = round(Number / 2);
     short Sum = 0;
 
-    for (int i = 1; i <= M; i++)
+    for (int i = 1; i < Number; i++)
     {
         if (Number % i == 0)
             Sum += i;
     }
-    if (Sum == Number)
-        return enPerfect_NotPerfect::Perfect;
-    else
-        return enPerfect_NotPerfect::Not_Perfect;
+    return(Sum == Number);
 }
 
 void PrintResults(short Number)
 {
-    if (CheckPerfectNumber(Number) == enPerfect_NotPerfect::Perfect)
+    if (IsPerfectNumber(Number))
         cout << "\n" << Number << " is a Perfect Number!\n";
     else
         cout << "\n" << Number << " is not a Perfect Number.\n";
