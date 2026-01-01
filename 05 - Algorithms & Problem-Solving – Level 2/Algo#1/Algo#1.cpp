9bcfb19105bd67a2873be4c4ac4c56e93313a507
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-01 19:04:01 +0300

    Algo#1

--- 05 - Algorithms & Problem-Solving – Level 2/Algo#1/Algo#1.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#1/Algo#1.cpp
@@ -6,14 +6,21 @@ using namespace std;
 void PrintHeader()
 {
     cout << "\t\t\tMultiplication Table From 1 to 10\n\n";
-    cout << "\t";
+    cout << "\t  ";
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << "\t";
+    }
 }
 
 int main()
 {
+    PrintHeader();
+    cout << endl;
     for (int i = 1; i <= 10; i++)
     {
-        cout << i << "\t|";
+        cout << i << "\t| ";
         for (int j = 1; j <= 10; j++)
         {
             cout << j * i << "\t";
