3989189b652b0ed46546d8429c77ca10dc975f4d
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-01 19:32:00 +0300

    Algo#2

--- 05 - Algorithms & Problem-Solving – Level 2/Algo#2/Algo#2.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#2/Algo#2.cpp
@@ -1,20 +1,44 @@
-﻿// Algo#2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
-#include <iostream>
+enum enPrime_NotPrime { Prime = 1, Not_Prime = 2 };
+
+short ReadNumber(string Message)
+{
+    short Number;
+    do
+    {
+        cout << Message; cin >> Number;
+
+    } while (Number < 0);
+
+    return Number;
+}
+
+enPrime_NotPrime CheckPrime(short Number)
+{
+    short M = round(Number / 2);
+
+    for (int i = 2; i <= M; i++)
+    {
+        if (Number % i == 0)
+            return enPrime_NotPrime::Not_Prime;
+        else
+            return enPrime_NotPrime::Prime;
+    }
+}
+
+void PrintPrimeNumbers(short Number)
+{
+    if (CheckPrime(Number) == enPrime_NotPrime::Prime)
+        cout << Number << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    PrintPrimeNumbers(CheckPrime(ReadNumber("Enter a Number: ")));
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
