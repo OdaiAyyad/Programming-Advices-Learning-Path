30114ecf27e19ca36a27b6f7e1cb788dc0d4c992
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-01 20:34:37 +0300

    Algo#3

--- 05 - Algorithms & Problem-Solving – Level 2/Algo#3/Algo#3.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#3/Algo#3.cpp
@@ -1,20 +1,49 @@
-﻿// Algo#3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
-#include <iostream>
+enum enPerfect_NotPerfect { Perfect = 1, Not_Perfect = 2 };
+
+short ReadPositiveNumber(string Message)
+{
+    short Number;
+    do
+    {
+        cout << Message; cin >> Number;
+
+    } while (Number < 0);
+
+    return Number;
+}
+
+enPerfect_NotPerfect CheckPerfectNumber(short Number)
+{
+    short M = round(Number / 2);
+    short Sum = 0;
+
+    for (int i = 1; i <= M; i++)
+    {
+        if (Number % i == 0)
+            Sum += i;
+    }
+    if (Sum == Number)
+        return enPerfect_NotPerfect::Perfect;
+    else
+        return enPerfect_NotPerfect::Not_Perfect;
+}
+
+void PrintResults(short Number)
+{
+    if (CheckPerfectNumber(Number) == enPerfect_NotPerfect::Perfect)
+        cout << "\n" << Number << " is a Perfect Number!\n";
+    else
+        cout << "\n" << Number << " is not a Perfect Number.\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    PrintResults(ReadPositiveNumber("Enter a Positive Number: "));
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
