5ce8cd5ecc098396760e3e72ce1dedd06ce65ca7
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-02 20:08:57 +0300

    Algo#6

--- 05 - Algorithms & Problem-Solving – Level 2/Algo#6/Algo#6.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#6/Algo#6.cpp
@@ -1,20 +1,41 @@
-﻿// Algo#6.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
-#include <iostream>
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+    do
+    {
+        cout << Message; cin >> Number;
+
+    } while (Number < 0);
+
+    return Number;
+}
+
+int DigitsSummation(int Number)
+{
+    int Remainder = 0, Sum = 0;
+
+    while (Number > 0)
+    {
+        Remainder = Number % 10;
+        Number = Number / 10;
+        Sum += Remainder;
+    }
+    return Sum;
+}
+
+void PrintSummation(int Sum)
+{
+    cout << "Sum of Digits = " << Sum << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    PrintSummation(DigitsSummation(ReadPositiveNumber("Enter a Positive Number: ")));
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
