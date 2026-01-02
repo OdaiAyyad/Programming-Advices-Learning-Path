742637dab2cfb872ef1cab14d0092d6397c9e059
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-02 22:27:26 +0300

    Algo#10

---
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#10/Algo#10.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+    do
+    {
+        cout << Message; cin >> Number;
+
+    } while (Number < 0);
+
+    return Number;
+}
+
+int ReverseNumber(int Number)
+{
+    int Remainder = 0, Number2 = 0;
+
+    while (Number > 0)
+    {
+        Remainder = Number % 10;
+        Number = Number / 10;
+        Number2 = Number2 * 10 + Remainder;
+    }
+    return Number2;
+}
+
+void PrintNumberDigits(int Number)
+{
+    int Remainder = 0;
+
+    while (Number > 0)
+    {
+        Remainder = Number % 10;
+        Number = Number / 10;
+        cout << Remainder << endl;
+    }
+}
+
+int main()
+{
+    PrintNumberDigits(ReverseNumber(ReadPositiveNumber("Enter a Positive Number: ")));
+
+    return 0;
+}
