5e5816193fd2f2c5c52b89086665b90676e6cde8
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-02 20:35:00 +0300

    Algo#8

---
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#8/Algo#8.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+    do
+    {
+        cout << Message; cin >> Number;
+
+    } while (Number < 0);
+
+    return Number;
+}
+
+short DigitFrequency(int Number, short Digit)
+{
+    short Counter = 0, Remainder = 0;
+
+    while (Number > 0)
+    {
+        Remainder = Number % 10;
+        Number = Number / 10;
+        if (Remainder == Digit)
+        {
+            Counter++;
+        }
+    }
+    return Counter;
+}
+
+int main()
+{
+    int Number = ReadPositiveNumber("Enter a Positive Number: ");
+    short Digit = ReadPositiveNumber("Enter a Digit: ");
+
+    cout << "\nDigit " << Digit << " Frequency is " << DigitFrequency(Number, Digit) << " Time(s).\n";
+
+
+    return 0;
+}
