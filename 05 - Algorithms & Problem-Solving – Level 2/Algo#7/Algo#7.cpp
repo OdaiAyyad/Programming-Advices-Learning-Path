5ff15f215fee0d93919ee7fd7fcf092180ff5256
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-02 20:20:07 +0300

    Algo#7

---
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#7/Algo#7.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+    do
+    {
+        cout << Message; cin >> Number;
+
+    } while (Number < 0);
+
+    return Number;
+}
+
+int ReverseNumber(int Number)
+{
+    int Remainder = 0, Number2 = 0;
+
+    while (Number > 0)
+    {
+        Remainder = Number % 10;
+        Number = Number / 10;
+        Number2 = Number2 * 10 + Remainder;
+    }
+    return Number2;
+}
+
+void PrintReversedNumber(int Number)
+{
+    cout << "\nReversed Number = " << Number << endl;
+}
+
+int main()
+{
+    PrintReversedNumber(ReverseNumber(ReadPositiveNumber("Enter a Positive Number: ")));
+
+    return 0;
+}
