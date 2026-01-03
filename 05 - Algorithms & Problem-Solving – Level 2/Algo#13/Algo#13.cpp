03629ef462a6faa1ea337946b2a8eb83546e9d40
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-03 21:57:53 +0300

    Algo#13

---
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#13/Algo#13.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+    int Number = 0;
+    do
+    {
+        cout << Message; cin >> Number;
+
+    } while (Number < 0);
+
+    return Number;
+}
+
+void PrintInvertedNumberPattern(int Number)
+{
+    for (int i = 1; i <= Number; i++)
+    {
+        for (int j = i; j >= 1; j--)
+        {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    PrintInvertedNumberPattern(ReadPositiveNumber("Enter a Positive Number: "));
+
+    return 0;
+}
