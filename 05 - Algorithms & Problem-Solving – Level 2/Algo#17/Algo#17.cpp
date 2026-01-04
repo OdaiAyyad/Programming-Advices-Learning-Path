2230ec98e9ef08a6588474e0649c0091d53f55b9
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-04 23:10:14 +0300

    Algo#17

---
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#17/Algo#17.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+string ReadPassword()
+{
+    string Password = "";
+
+    cout << "Enter a 3-Letters Password (All Capital): "; cin >> Password;
+
+    return Password;
+}
+
+bool PasswordSearch(string OriginalPassword)
+{
+    short Counter = 0; string Password = "";
+
+    for (int i = 65; i <= 90; i++)
+    {
+        for (int j = 65; j <= 90; j++)
+        {
+            for (int k = 65; k <= 90; k++)
+            {
+                Counter++;
+
+                Password = Password + char(i);
+                Password = Password + char(j);
+                Password = Password + char(k);
+
+                if (OriginalPassword == Password)
+                {
+                    cout << "Password is " << Password << endl;
+                    cout << "Found after " << Counter << " Trial(s).\n";
+
+                    return true;
+                }
+                else
+                    Password = "";
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    PasswordSearch(ReadPassword());
+
+    return 0;
+}
