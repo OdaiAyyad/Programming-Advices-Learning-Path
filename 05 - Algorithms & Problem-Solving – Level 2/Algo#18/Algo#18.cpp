f1c2f6e1d00a820d5f8b359984d73626d5bb4035
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-04 23:25:20 +0300

    Algo#18

--- 05 - Algorithms & Problem-Solving – Level 2/Algo#18/Algo#18.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#18/Algo#18.cpp
@@ -1,20 +1,32 @@
-﻿// Algo#18.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
-#include <iostream>
+string ReadName()
+{
+    string Name = "";
+
+    cout << "Enter your Name: "; cin >> Name;
+
+    return Name;
+}
+
+string Encryption(string Name)
+{
+    for (int i = 0; i <= Name.length(); i++)
+    {
+        Name[i] += 2;
+    }
+    
+    return Name;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string Name = ReadName();
+    cout << "Name Before Encryption: " << Name << endl;
+    cout << "Name After Encryption: " << Encryption(Name) << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
