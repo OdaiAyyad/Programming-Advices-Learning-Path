ea67266b30af5ba208346148e5b836d8d9db002a
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-05 20:34:31 +0300

    Algo#20

---
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#21/Algo#21.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+enum enCharType { SmallLetter = 1, CapitalLetter = 2, SpecialCharacter = 3, Digit = 4 };
+
+short ReadNumber(string Message)
+{
+    short Number;
+    do
+    {
+        cout << Message; cin >> Number;
+
+    } while (Number < 0);
+
+    return Number;
+}
+
+int RandomNumber(int From, int To)
+{
+    // Function to generate a random number
+    int RandNum = rand() % (To - From + 1) + From;
+
+    return RandNum;
+}
+
+char GetRandomCharacter(enCharType CharType)
+{
+    switch (CharType)
+    {
+    case enCharType::SmallLetter:
+        return char(RandomNumber(97, 122));
+
+    case enCharType::CapitalLetter:
+        return char(RandomNumber(65, 90));
+
+    case enCharType::SpecialCharacter:
+        return char(RandomNumber(33, 47));
+
+    case enCharType::Digit:
+        return char(RandomNumber(48, 57));
+    }
+}
+
+string GenerateWord(enCharType CharType, short Length)
+{
+    string Word;
+
+    for (int i = 1; i <= Length; i++)
+    {
+        Word = Word + GetRandomCharacter(CharType);
+    }
+    return Word;
+}
+
+string GenerateKey()
+{
+    string Key = "";
+
+    Key = GenerateWord(enCharType::CapitalLetter, 4) + "-";
+    Key = Key + GenerateWord(enCharType::CapitalLetter, 4) + "-";
+    Key = Key + GenerateWord(enCharType::CapitalLetter, 4) + "-";
+    Key = Key + GenerateWord(enCharType::CapitalLetter, 4);
+
+    return Key;
+}
+
+void GenerateKeys(short NumberOfKeys)
+{
+    for (int i = 1; i < +NumberOfKeys; i++)
+    {
+        cout << "Key [" << i << "]: ";
+        cout << GenerateKey();
+    }
+}
+
+int main()
+{
+    // Seeds the random number generator in C++, called only once
+    srand(unsigned(time(NULL)));
+
+    GenerateKeys(ReadNumber("Enter a Number: "));
+
+    return 0;
+}
