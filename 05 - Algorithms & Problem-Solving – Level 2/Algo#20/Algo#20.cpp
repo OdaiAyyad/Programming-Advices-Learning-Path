40ac65ba510788b56f03f596b80a1c8a8985775d
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-05 20:03:19 +0300

    Algo#20

--- 05 - Algorithms & Problem-Solving – Level 2/Algo#20/Algo#20.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#20/Algo#20.cpp
@@ -1,20 +1,37 @@
-﻿// Algo#20.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
 
-#include <iostream>
+enum enCharType { SmallLetter = 1, CapitalLetter = 2, SpecialCharacter = 3, Digit = 4 };
+
+int RandomNumber(int From, int To)
+{
+    // Function to generate a random number
+    int RandNum = rand() % (To - From + 1) + From;
+
+    return RandNum;
+}
+
+char GetRandomCharacter(enCharType CharType)
+{
+    switch (CharType)
+    {
+    case enCharType::SmallLetter:
+        return RandomNumber(char(97), char(122));
+    case enCharType::CapitalLetter:
+        return char(RandomNumber(65, 90));
+
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Seeds the random number generator in C++, called only once
+    srand(unsigned(time(NULL)));
+
+    cout << GetRandomCharacter(enCharType::SmallLetter) << endl;
+    cout << GetRandomCharacter(enCharType::CapitalLetter) << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
