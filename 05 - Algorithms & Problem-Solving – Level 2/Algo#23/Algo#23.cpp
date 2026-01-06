26e35b2cd67d88791e8f2aa74238eeb2dda7a9e6
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-06 22:36:19 +0300

    Algo#23

--- 05 - Algorithms & Problem-Solving – Level 2/Algo#23/Algo#23.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#23/Algo#23.cpp
@@ -3,6 +3,18 @@
 #include <cmath>
 using namespace std;
 
+int ReadPositiveNumber(string Message)
+{
+    int Number;
+    do
+    {
+        cout << Message; cin >> Number;
+
+    } while (Number <= 0);
+
+    return Number;
+}
+
 int main()
 {
 
