7e50a426bc7799947bc55ccc6b870bd535623bac
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-10 21:59:13 +0300

    Algo#31

--- 05 - Algorithms & Problem-Solving – Level 2/Algo#31/Algo#31.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#31/Algo#31.cpp
@@ -1,20 +1,76 @@
-﻿// Algo#31.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
-#include <iostream>
+int ReadPositiveNumber(string Message)
+{
+    int Number;
+    do
+    {
+        cout << Message; cin >> Number;
+
+    } while (Number <= 0);
+
+    return Number;
+}
+
+int RandomNumber(int From, int To)
+{
+    int RandNum = rand() % (To - From + 1) + From;
+
+    return RandNum;
+}
+
+void Swap(int& A, int& B)
+{
+    int Temp;
+
+    Temp = A;
+    A = B;
+    B = Temp;
+}
+
+void FillArrayWith1ToN(int arr[100], int ArrayLength)
+{
+    for (int i = 0; i < ArrayLength; i++)
+    {
+        arr[i] = i + 1;
+    }
+}
+
+void ShuffleArray(int arr[100], int ArrayLength)
+{
+    for (int i = 0; i < ArrayLength; i++)
+    {
+        Swap(arr[RandomNumber(1, ArrayLength) - 1], arr[RandomNumber(1, ArrayLength) - 1]);
+    }
+}
+
+void PrintArray(int arr[100], int ArrayLength)
+{
+    for (int i = 0; i < ArrayLength; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand((unsigned)time(NULL));
+
+    int arr[100], ArrayLength(ReadPositiveNumber("Enter a Number: "));
+
+    FillArrayWith1ToN(arr, ArrayLength);
+
+    cout << "\nArray Elements: ";
+    PrintArray(arr, ArrayLength);
+
+    ShuffleArray(arr, ArrayLength);
+
+    cout << "\nArray Elements after shuffle: ";
+    PrintArray(arr, ArrayLength);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
