f5896b95e0c5fa4b18f81e2d58a93f56d6851e5a
Odai Ayyad <3odaysuheel10@gmail.com>
2026-01-15 23:52:53 +0300

    Algo#33

--- 05 - Algorithms & Problem-Solving – Level 2/Algo#33/Algo#33.cpp
+++ 05 - Algorithms & Problem-Solving – Level 2/Algo#33/Algo#33.cpp
@@ -1,20 +1,108 @@
-﻿// Algo#33.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
-#include <iostream>
+enum enCharType { SmallLetter = 1, CapitalLetter = 2, SpecialCharacter = 3, Digit = 4 };
+
+int ReadPositiveNumber(string Message)
+{
+    int Number;
+    do
+    {
+        cout << Message; cin >> Number;
+
+    } while (Number <= 0);
+
+    return Number;
+}
+
+int RandomNumber(int From, int To)
+{
+    int RandNum = rand() % (To - From + 1) + From;
+
+    return RandNum;
+}
+
+char GetRandomCharacter(enCharType CharType)
+{
+    switch (CharType)
+    {
+    case enCharType::SmallLetter:
+        return char(RandomNumber(97, 122));
+
+    case enCharType::CapitalLetter:
+        return char(RandomNumber(65, 90));
+
+    case enCharType::SpecialCharacter:
+        return char(RandomNumber(33, 47));
+
+    case enCharType::Digit:
+        return char(RandomNumber(48, 57));
+    }
+}
+
+string GenerateWord(enCharType CharType, short Length)
+{
+    string Word;
+
+    for (int i = 1; i <= Length; i++)
+    {
+        Word = Word + GetRandomCharacter(CharType);
+    }
+    return Word;
+}
+
+string GenerateKey()
+{
+    string Key = "";
+
+    Key = GenerateWord(enCharType::CapitalLetter, 4) + "-";
+    Key = Key + GenerateWord(enCharType::CapitalLetter, 4) + "-";
+    Key = Key + GenerateWord(enCharType::CapitalLetter, 4) + "-";
+    Key = Key + GenerateWord(enCharType::CapitalLetter, 4);
+
+    return Key;
+}
+
+void GenerateKeys(short NumberOfKeys)
+{
+    cout << "\n";
+
+    for (int i = 1; i <= NumberOfKeys; i++)
+    {
+        cout << "Key [" << i << "]: ";
+        cout << GenerateKey() << endl;
+    }
+}
+
+void FillArrayWithRandomKeys(int arr[100], int ArrayLength)
+{
+    for (int i = 0; i < ArrayLength; i++)
+    {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+
+void PrintArray(int arr[100], int ArrayLength)
+{
+    for (int i = 0; i < ArrayLength; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand((unsigned)time(NULL));
+
+    int arr[100], ArrayLength(ReadPositiveNumber("Enter a Number: "));
+
+    FillArrayWithRandomKeyrs(arr, ArrayLength);
+
+    cout << "\nArray Elements: ";
+    PrintArray(arr, ArrayLength);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
